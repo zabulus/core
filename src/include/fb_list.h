1179e739195dd052b3a0f9a0a3b48f0899acdda8
tamlin <tamlin@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-06-12 06:58:00 +0000

    There is no C++ class named slist. Removed.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@482 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/include/fb_list.h
+++ src/include/fb_list.h
@@ -4,17 +4,11 @@
 #include "../common/memory/allocators.h"
 
 #include <list>
-#include <slist>
 
 namespace Firebird
 {
 	template <class T>
 	class list : public std::list<T, Firebird::allocator<T> >
-	{
-	};
-
-	template <class T>
-	class slist : public std::slist<T, Firebird::allocator<T> >
 	{
 	};
 };
