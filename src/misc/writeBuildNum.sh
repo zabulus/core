0dd86fc83d8eabbfd652dede2bcb0645082df642
tamlin <tamlin@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-06-12 07:02:09 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@486 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.316 2002-06-12 07:01:27 tamlin Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.317 2002-06-12 07:02:09 tamlin Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=345
+BuildNum=346
 BuildSuffix="Firebird2 Dev1"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
