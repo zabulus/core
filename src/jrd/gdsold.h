398a4840460d69ec86dbf73a5861d3f6e9b042f7
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-07-03 07:16:33 +0000

    Merging changes from fb1 (stage 2 - fixing porting errors).
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@642 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/gdsold.h
+++ src/jrd/gdsold.h
@@ -1010,7 +1010,7 @@ const char gds_info_db_class_cache = 12;
 #define gds__info_rsb_sim_cross		  19
 #define gds__info_rsb_once		  20
 #define gds__info_rsb_procedure		  21
-#define gds__info_rsb_procedure		  22
+#define gds__info_rsb_skip		  22
 
 /**********************/
 /* bitmap expressions */
