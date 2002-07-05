c8e29491c25d80224433a23b685c6d75246d8c57
skywalker <skywalker@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-07-05 12:07:14 +0000

    Change to have print build num option
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@659 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,7 +3,7 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.419 2002-07-05 10:09:08 pcisar Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.420 2002-07-05 12:07:14 skywalker Exp $"
 
 BuildType=T
 MajorVer=2
@@ -65,4 +65,7 @@ eof
 if [ "$1" = "rebuildHeader" ]
   then
     rebuildHeaderFile
+elif [ "$1" = "--version" ]
+  then
+    echo "Build Version : " $PRODUCT_VER_STRING $BuildSuffix
 fi
