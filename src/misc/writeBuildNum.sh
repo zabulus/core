88d71715f8e1df9ca8ca7477e227cf1e8ac336f5
fsg <fsg@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-10-02 13:26:23 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@1381 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.867 2002-10-02 13:24:59 dimitr Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.868 2002-10-02 13:26:23 fsg Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=5
 RevNo=0
-BuildNum=887
+BuildNum=888
 BuildSuffix="Firebird 1.5 Alpha 1"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
