16838f323b5dffbae87e79b946cca5cf7746a723
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-10-31 12:01:53 +0000

    Disallowed Nickolay's SAVEPOINT feature for SPs/triggers.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@1835 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/dsql/pass1.cpp
+++ src/dsql/pass1.cpp
@@ -1376,7 +1376,16 @@ NOD PASS1_statement(REQ request, NOD input, USHORT proc_flag)
 		return input;
 		
 	case nod_user_savepoint:
+		if (request->req_flags & REQ_procedure)
+			ERRD_post(gds_sqlerr, gds_arg_number, (SLONG) - 104, gds_arg_gds, gds_token_err,	/* Token unknown */
+					  gds_arg_gds, gds_random, gds_arg_string, "SAVEPOINT", 0);
+		request->req_type = REQ_SAVEPOINT;	
+		return input;
+
 	case nod_undo_savepoint:
+		if (request->req_flags & REQ_procedure)
+			ERRD_post(gds_sqlerr, gds_arg_number, (SLONG) - 104, gds_arg_gds, gds_token_err,	/* Token unknown */
+					  gds_arg_gds, gds_random, gds_arg_string, "ROLLBACK", 0);
 		request->req_type = REQ_SAVEPOINT;	
 		return input;
 
