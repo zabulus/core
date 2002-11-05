9350f0664a6c7578c87775a5e09ae1e2b23fc0b2
nmcc <nmcc@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-11-05 01:49:38 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@1951 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.1200 2002-11-05 01:42:48 nmcc Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.1201 2002-11-05 01:49:38 nmcc Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=5
 RevNo=0
-BuildNum=1218
+BuildNum=1219
 BuildSuffix="Firebird 1.5 Alpha 4"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
