6f05f9b53252f458e56b6438d1720e63a7ea62ca
eku <eku@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-11-21 10:20:06 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@2572 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.1550 2002-11-21 10:08:43 eku Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.1551 2002-11-21 10:20:06 eku Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=5
 RevNo=0
-BuildNum=1568
+BuildNum=1569
 BuildSuffix="Firebird 1.5 Alpha 4"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
