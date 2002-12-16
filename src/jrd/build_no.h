85f7e19db313fc5c69bfc7d51bb5bb6adca21a8b
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-12-16 21:19:37 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@2974 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:1795 
+  FORMAL BUILD NUMBER:1796 
 */
 
-#define PRODUCT_VER_STRING "1.5.0.1795"
-#define FILE_VER_STRING "WI-T1.5.0.1795"
-#define LICENSE_VER_STRING "WI-T1.5.0.1795"
-#define FILE_VER_NUMBER 1, 5, 0, 1795
+#define PRODUCT_VER_STRING "1.5.0.1796"
+#define FILE_VER_STRING "WI-T1.5.0.1796"
+#define LICENSE_VER_STRING "WI-T1.5.0.1796"
+#define FILE_VER_NUMBER 1, 5, 0, 1796
 #define FB_MAJOR_VER "1"
 #define FB_MINOR_VER "5"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "1795"
+#define FB_BUILD_NO "1796"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 1.5 Alpha 5"
