cb6df7f8ed272ee3e19f7cefa1404ce4ba0ee33b
nmcc <nmcc@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2002-12-30 15:46:47 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@3065 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:1842 
+  FORMAL BUILD NUMBER:1843 
 */
 
-#define PRODUCT_VER_STRING "1.5.0.1842"
-#define FILE_VER_STRING "WI-T1.5.0.1842"
-#define LICENSE_VER_STRING "WI-T1.5.0.1842"
-#define FILE_VER_NUMBER 1, 5, 0, 1842
+#define PRODUCT_VER_STRING "1.5.0.1843"
+#define FILE_VER_STRING "WI-T1.5.0.1843"
+#define LICENSE_VER_STRING "WI-T1.5.0.1843"
+#define FILE_VER_NUMBER 1, 5, 0, 1843
 #define FB_MAJOR_VER "1"
 #define FB_MINOR_VER "5"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "1842"
+#define FB_BUILD_NO "1843"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 1.5 Alpha 5"
