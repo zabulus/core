436235aa18412911178653f8b8c9105e1500b62b
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-07 12:58:30 +0000

    Make default parameters values for non forced writes mode depend on the platform
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@3633 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/config/config.cpp
+++ src/common/config/config.cpp
@@ -91,8 +91,13 @@ const ConfigImpl::ConfigEntry ConfigImpl::entries[] =
 	{TYPE_INTEGER,		"RemoteServicePort",		(ConfigValue) FB_SERVICE_PORT},
 	{TYPE_STRING,		"RemotePipeName",			(ConfigValue) FB_PIPE_NAME},
 	{TYPE_STRING,		"IpcName",					(ConfigValue) FB_IPC_NAME},
+#ifdef WIN_NT
 	{TYPE_STRING,		"MaxUnflushedWrites",		(ConfigValue) 100},
 	{TYPE_STRING,		"MaxUnflushedWriteTime",	(ConfigValue) 5}
+#else
+	{TYPE_STRING,		"MaxUnflushedWrites",		(ConfigValue) -1},
+	{TYPE_STRING,		"MaxUnflushedWriteTime",	(ConfigValue) -1}
+#endif
 };
 
 /******************************************************************************
