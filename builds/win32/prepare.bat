f4c878f02fc7843320a3d448c3022d8f5f1fa3c3
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-10 12:00:23 +0000

    Batch file changed to use new extension of the database files.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@3741 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/prepare.bat
+++ builds/win32/prepare.bat
@@ -6,27 +6,28 @@
 @if "%1"=="" (@goto :ERROR_PARAM_EXPECTED)
 @md dbs
 @cd dbs
-@gbak -r %1\src\misc\metadata.gbak localhost:%1\builds\win32\dbs\metadata.gdb
+@gbak -r %1\src\misc\metadata.gbak localhost:%1\builds\win32\dbs\metadata.fdb
 @md jrd
-@gbak -r %1\src\misc\security.gbak localhost:%1\builds\win32\dbs\jrd\isc.gdb
+@gbak -r %1\src\misc\security.gbak localhost:%1\builds\win32\dbs\jrd\security.fdb
 @md msgs
-@gbak -r %1\src\msgs\msg.gbak localhost:%1\builds\win32\dbs\msgs\msg.gdb
+@gbak -r %1\src\msgs\msg.gbak localhost:%1\builds\win32\dbs\msgs\msg.fdb
 @md qli
-@gbak -r %1\src\misc\help.gbak localhost:%1\builds\win32\dbs\qli\help.gdb
+@gbak -r %1\src\misc\help.gbak localhost:%1\builds\win32\dbs\qli\help.fdb
 @cd ..
 @del expand.sed
-@call :SED yachts.lnk %1 metadata.gdb
-@call :SED ODS.RDB %1 jrd/isc.gdb
-@call :SED msg.gdb %1 msgs/msg.gdb
-@call :SED master_msg_db %1 msgs/msg.gdb
-@call :SED yachts.link %1 jrd/isc.gdb
-@call :SED master_help_db %1 qli/help.gdb
-@call :SED help.gdb %1 qli/help.gdb
-@call :SED yachts.gdb %1 jrd/isc.gdb
-@call :SED isc.gdb %1 jrd/isc.gdb
-@call :SED isc4.gdb %1 jrd/isc.gdb
-@call :SED empbuild.gdb %1 example5/employee.gdb
-@call :SED source/msgs/msg.gdb %1 msgs/msg.gdb
+@call :SED yachts.lnk %1 metadata.fdb
+@call :SED ODS.RDB %1 jrd/security.fdb
+@call :SED msg.gdb %1 msgs/msg.fdb
+@call :SED master_msg_db %1 msgs/msg.fdb
+@call :SED yachts.link %1 jrd/security.fdb
+@call :SED master_help_db %1 qli/help.fdb
+@call :SED help.gdb %1 qli/help.fdb
+@call :SED yachts.gdb %1 jrd/security.fdb
+@call :SED isc.gdb %1 jrd/security.fdb
+@call :SED isc4.gdb %1 jrd/security.fdb
+@call :SED security.fdb %1 jrd/security.fdb
+@call :SED empbuild.gdb %1 example5/employee.fdb
+@call :SED source/msgs/msg.gdb %1 msgs/msg.fdb
 @goto :END
 
 ::===========
