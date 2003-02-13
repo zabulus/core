96d6a50dc22e44dfe57d093770d240c210da3d08
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-13 13:09:29 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@3969 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:2335 
+  FORMAL BUILD NUMBER:2336 
 */
 
-#define PRODUCT_VER_STRING "1.5.0.2335"
-#define FILE_VER_STRING "WI-T1.5.0.2335"
-#define LICENSE_VER_STRING "WI-T1.5.0.2335"
-#define FILE_VER_NUMBER 1, 5, 0, 2335
+#define PRODUCT_VER_STRING "1.5.0.2336"
+#define FILE_VER_STRING "WI-T1.5.0.2336"
+#define LICENSE_VER_STRING "WI-T1.5.0.2336"
+#define FILE_VER_NUMBER 1, 5, 0, 2336
 #define FB_MAJOR_VER "1"
 #define FB_MINOR_VER "5"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "2335"
+#define FB_BUILD_NO "2336"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 1.5 Beta 2"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.2320 2003-02-13 13:02:05 dimitr Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.2321 2003-02-13 13:09:29 dimitr Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=5
 RevNo=0
-BuildNum=2335
+BuildNum=2336
 BuildSuffix="Firebird 1.5 Beta 2"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
