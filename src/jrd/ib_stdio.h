9fae65ebcf9a2949e7fd9b224db3a13a22a25413
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-13 13:42:04 +0000

    Remove some unused defines & macros
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@3974 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/ib_stdio.h
+++ src/jrd/ib_stdio.h
@@ -350,7 +350,7 @@ __END_DECLS
  */
 #define	ib__sgetc(p) (--(p)->_r < 0 ? ib__srget(p) : (int)(*(p)->_p++))
 #if defined(__GNUC__) && defined(__STDC__)
-	 static __inline int ib__sputc(int _c, IB_FILE * _p)
+	 static inline int ib__sputc(int _c, IB_FILE * _p)
 {
 	if (--_p->_w >= 0 || (_p->_w >= _p->_lbfsize && (char) _c != '\n'))
 		return (*_p->_p++ = _c);
