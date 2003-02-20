55a64107d1262f9903cdaf5cdb10f3d5fd42eb03
fsg <fsg@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-20 20:25:28 +0000

    Blas, could you please stop to replace warnings with errors :-) main needs to return int
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@4428 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/qli/dtr.cpp
+++ src/qli/dtr.cpp
@@ -104,7 +104,7 @@ static struct answer_t answer_table[] = {
 };
 
 
-void CLIB_ROUTINE main( int argc, char **argv)
+int  CLIB_ROUTINE main( int argc, char **argv)
 {
 /**************************************
  *
