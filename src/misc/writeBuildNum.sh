3aadd323ba5ff7ed58d49e6d461f78cd3cd25e5c
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-02-22 17:21:23 +0000

    Beta 3 will be out in a few days.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@4464 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,14 +3,14 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.2599 2003-02-22 17:13:12 dimitr Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.2600 2003-02-22 17:21:23 dimitr Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=5
 RevNo=0
 BuildNum=2614
-BuildSuffix="Firebird 1.5 Beta 2"
+BuildSuffix="Firebird 1.5 Beta 3"
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
 PRODUCT_VER_STRING="$MajorVer.$MinorVer.$RevNo.$BuildNum"
