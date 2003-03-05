9c93217b9ecdc9cc547db50979802d751650c100
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-03-05 23:36:00 +0000

    Changes for MINGW build -Correct bad function declaration.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@4856 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/utl_proto.h
+++ src/jrd/utl_proto.h
@@ -37,7 +37,7 @@ extern "C" {
 #endif
 
 extern int API_ROUTINE gds__blob_size(FRBRD **, SLONG *, SLONG *, SLONG *);
-extern void API_ROUTINE isc_expand_dpb(SCHAR **, SSHORT *, ...);
+extern void API_ROUTINE_VARARG isc_expand_dpb(SCHAR **, SSHORT *, ...);
 extern int API_ROUTINE gds__edit(TEXT *, USHORT);
 extern SLONG API_ROUTINE gds__event_block(SCHAR **, SCHAR **, USHORT, ...);
 extern USHORT API_ROUTINE gds__event_block_a(SCHAR **, SCHAR **, SSHORT,
