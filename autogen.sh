8e403796516cf95de6cf447156fa854c9e910e4a
eku <eku@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-03-07 07:41:13 +0000

    Started to use libtool (http://www.gnu.org/software/libtool/). It will eventually handle all platform specific compile and link issues! Removed generated files from the view (config.guess and config.sub).
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@4905 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,13 @@
 #
 # Run this to generate all the initial makefiles, etc.
 #
-# $Id: autogen.sh,v 1.5 2002-11-05 01:36:39 nmcc Exp $
+# $Id: autogen.sh,v 1.6 2003-03-07 07:41:13 eku Exp $
 
 PKG_NAME=Firebird2
 SRCDIR=`dirname $0`
 DIE=0
 
-VER=` autoconf --version | grep '^[Aa]utoconf'|sed 's/^[^0-9]*//'`
+VER=`autoconf --version|grep '^[Aa]utoconf'|sed 's/^[^0-9]*//'`
 case "$VER" in
  0* | 1\.* | 2\.[0-9] | 2\.[0-9][a-z]* | \
  2\.[1-4][0-9] | 2\.5[0-2][a-z]* )
@@ -16,6 +16,18 @@ case "$VER" in
   echo "**Error**: You must have autoconf 2.53 or later installed."
   echo "Download the appropriate package for your distribution/OS,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/"
+  DIE=1
+  ;;
+esac
+
+VER=`libtool --version|grep ' libtool)' | sed 's/.*) \([0-9][0-9.]*\) .*/\1/'`
+case "$VER" in
+ 0* | 1\.[0-2] | 1\.[0-2][a-z]* | \
+ 1\.3\.[0-2] | 1\.3\.[0-2][a-z]* )
+  echo
+  echo "**Error**: You must have libtool 1.3.3 or later installed."
+  echo "Download the appropriate package for your distribution/OS,"
+  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/"
   DIE=1
   ;;
 esac
@@ -37,6 +49,8 @@ if test -z "$*" -a x$NOCONFIGURE = x; then
 fi
 
 # Generate configure from configure.in
+echo "Running libtoolize ..."
+libtoolize --copy --force || exit 1
 echo "Running autoconf ..."
 autoconf || exit 1
 
