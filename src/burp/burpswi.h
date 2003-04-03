79a2f2f31f0d185afad665005d29c7ecc5816b7b
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-04-03 08:51:16 +0000

    Move definitions only used by gspit to spit.cpp
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@5383 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/burp/burpswi.h
+++ src/burp/burpswi.h
@@ -164,18 +164,5 @@ static struct in_sw_tab_t burp_in_sw_table [] =
 };
 
 
-/* Definitions for GSPLIT */
-#define	IN_SW_SPIT_0	0   /* the unknowable switch */
-#define	IN_SW_SPIT_SP	30  /* splits back up files */
-#define	IN_SW_SPIT_JT	31  /* joins back up files */
-
-
-static struct in_sw_tab_t spit_in_sw_table [] =
-{
-	{IN_SW_SPIT_SP,	0,	"SPLIT_BK_FILE", 0, 0, 0, FALSE, 0, 0, NULL},
-	{IN_SW_SPIT_JT,	0,	"JOIN_BK_FILE",	 0, 0, 0, FALSE, 0, 0, NULL},
-	{IN_SW_SPIT_0,	0,	NULL,			 0, 0, 0, FALSE, 0, 0, NULL}
-};
-
 #endif /* _BURP_BURP_H_ */
 
