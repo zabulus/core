aa869c3f2940ff5cdacc8f7584a61cac6f438e53
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-05-08 19:47:27 +0000

    Optimize it a little
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@5883 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/intl/lc_ascii.c
+++ src/intl/lc_ascii.c
@@ -498,7 +498,7 @@ TEXTTYPE_ENTRY(NEXT_c0_init)
 #define CP1251_UPPER_EX4 0xB2	// ²
 #define CP1251_LOWER_EX4 0xB3	// ³
 
-UCHAR CP1251_UPPER(UCHAR ch)
+static inline UCHAR CP1251_UPPER(UCHAR ch)
 {
 	UCHAR res;
 
@@ -537,7 +537,7 @@ UCHAR CP1251_UPPER(UCHAR ch)
 	return res;
 }
 
-UCHAR CP1251_LOWER(UCHAR ch)
+static inline UCHAR CP1251_LOWER(UCHAR ch)
 {
 	UCHAR res;
 
