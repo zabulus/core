1ed2fd22ebc2adf66704b82be2e812c97709b250
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-06-08 18:12:13 +0000

    Re-enable default asymmetric protocol.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@6154 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/remote/protocol.h
+++ src/remote/protocol.h
@@ -32,14 +32,14 @@
  *
  */
 /*
-$Id: protocol.h,v 1.11 2003-05-02 17:17:09 dimitr Exp $
+$Id: protocol.h,v 1.12 2003-06-08 18:12:13 dimitr Exp $
 */
 #ifndef _REMOTE_PROTOCOL_H_
 #define _REMOTE_PROTOCOL_H_
 
 // dimitr: ask for asymmetric protocols only.
 // Comment it out to return back to FB 1.0 behaviour.
-//#define ASYMMETRIC_PROTOCOLS_ONLY
+#define ASYMMETRIC_PROTOCOLS_ONLY
 
 /* The protocol is defined blocks, rather than messages, to
    separate the protocol from the transport layer.  */
