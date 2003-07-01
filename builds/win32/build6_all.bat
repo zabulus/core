55939e37586944a974add8dbc277ea09fb89dc32
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-07-01 19:20:38 +0000

    New dsp's to replace nmake's
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@6307 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

---
+++ builds/win32/build6_all.bat
@@ -0,0 +1,61 @@
+
+
+:: @echo off
+
+:CHECK_ENV
+@if /I "%DB_PATH%"=="" (goto :HELP & goto :EOF) else (@goto :MAIN)
+
+::===========
+:MAIN
+@echo.
+@del ..\..\src\include\gen\autoconfig.h
+@copy ..\..\src\include\gen\autoconfig_msvc.h ..\..\src\include\gen\autoconfig.h
+@echo Preprocessing files required to do a boot-build...
+@del gpre_boot.exe
+@cd msvc6
+@msdev Firebird2.dsw /MAKE "common_static - Win32 Release" "gpre_boot - Win32 Release"  /REBUILD /OUT boot1.log
+@cd ..
+@move msvc6\release\firebird\bin\gpre_boot.exe .
+@move msvc6\release\common_static\common_static.lib temp\common_static.lib
+@call preprocess.bat BOOT
+
+@echo.
+@echo Making necessary boot-build executables...
+@cd msvc6
+@msdev Firebird2.dsw /MAKE "fbclient_static - Win32 Release" "gpre_static - Win32 Release"  /REBUILD /OUT boot2.log
+@cd ..
+@echo Move gpre_static and fbclient_static 
+@move msvc6\release\firebird\bin\gpre_static.exe .
+
+@move msvc6\release\fbclient_static\fbclient_static.lib temp\fbclient_static.lib
+@echo Preprocessing the entire source tree...
+@call preprocess.bat
+@echo Building message file and codes header...
+@cd msvc6
+@msdev Firebird2.dsw /MAKE "build_msg - Win32 Release" "codes - Win32 Release"  /REBUILD /OUT boot3.log
+@cd ..
+@move msvc6\release\build_msg\build_msg.exe .
+@move msvc6\release\codes\codes.exe .
+@build_msg
+@codes ..\..\src\include\gen
+@echo Building BLR Table
+@call blrtable.bat
+@goto :NEXT_STEP
+
+::==============
+:NEXT_STEP
+@echo.
+@echo    You may now run make6_debug.bat or make6_release.bat
+@echo.
+@goto :EOF
+
+::==============
+:HELP
+@echo.
+@echo    You must run prepare.bat before running this script
+@echo.
+@goto :EOF
+
+
+::==============
+:END
