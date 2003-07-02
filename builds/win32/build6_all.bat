890f1b56c4c026b38798bf4089df8e53c190e7e2
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-07-02 11:42:16 +0000

    New dsp's to replace nmake's (split boot workspace)
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@6325 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/build6_all.bat
+++ builds/win32/build6_all.bat
@@ -15,7 +15,7 @@
 @echo Preprocessing files required to do a boot-build...
 @del gpre_boot.exe
 @cd msvc6
-@msdev Firebird2.dsw /MAKE "common_static - Win32 Release" "gpre_boot - Win32 Release"  /REBUILD /OUT boot1.log
+@msdev Firebird2Boot.dsw /MAKE "common_static - Win32 Release" "gpre_boot - Win32 Release"  /REBUILD /OUT boot1.log
 @cd ..
 @move msvc6\release\firebird\bin\gpre_boot.exe .
 @move msvc6\release\common_static\common_static.lib temp\common_static.lib
@@ -24,7 +24,7 @@
 @echo.
 @echo Making necessary boot-build executables...
 @cd msvc6
-@msdev Firebird2.dsw /MAKE "fbclient_static - Win32 Release" "gpre_static - Win32 Release"  /REBUILD /OUT boot2.log
+@msdev Firebird2Boot.dsw /MAKE "fbclient_static - Win32 Release" "gpre_static - Win32 Release"  /REBUILD /OUT boot2.log
 @cd ..
 @echo Move gpre_static and fbclient_static 
 @move msvc6\release\firebird\bin\gpre_static.exe .
@@ -34,7 +34,7 @@
 @call preprocess.bat
 @echo Building message file and codes header...
 @cd msvc6
-@msdev Firebird2.dsw /MAKE "build_msg - Win32 Release" "codes - Win32 Release"  /REBUILD /OUT boot3.log
+@msdev Firebird2Boot.dsw /MAKE "build_msg - Win32 Release" "codes - Win32 Release"  /REBUILD /OUT boot3.log
 @cd ..
 @move msvc6\release\build_msg\build_msg.exe .
 @move msvc6\release\codes\codes.exe .
