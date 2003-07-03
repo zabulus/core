3cee1b825663ccc70e43578ce0ef790aec56dab5
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-07-03 13:08:10 +0000

    Add batch to remove build created files and directories
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@6364 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/clean_all.bat
+++ builds/win32/clean_all.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@echo Cleaning build
+@echo Cleaning directories
 @rmdir /S /Q dbs 2>nul
 @rmdir /S /Q temp 2>nul
 @rmdir /S /Q output 2>nul
@@ -9,8 +9,14 @@
 @rmdir /S /Q msvc7\debug 2>nul
 @rmdir /S /Q msvc7\release 2>nul
 @rmdir /S /Q ..\..\gen 2>nul
+
+@echo Cleaning files
 @del *.exe 2>nul
 @del *.msg 2>nul
+@del *.dll 2>nul
 @del ..\..\src\include\gen\autoconfig.h 2>nul
 @del ..\..\src\include\gen\blrtable.h 2>nul
-@del msvc6\*.log
+@del msvc6\*.log 2>nul
+@del msvc6\*.plg 2>nul
+@del msvc7\*.log 2>nul
+@del msvc7\*.plg 2>nul
