0587cce8f25ec14ac23f08baddaa22cbcc8a4d24
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-07-08 23:05:09 +0000

    Moving install from 'src' to 'builds'
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@6631 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/clean_all.bat
+++ builds/win32/clean_all.bat
@@ -8,13 +8,11 @@
 @rmdir /S /Q %ROOT_PATH%\gen 2>nul
 @rmdir /S /Q %ROOT_PATH%\temp 2>nul
 
-@echo Cleaning build files
+@echo Cleaning and install build files
 @del *.log 2>nul
 @del %ROOT_PATH%\src\include\gen\autoconfig.h 2>nul
 @del %ROOT_PATH%\src\include\gen\blrtable.h 2>nul
 @del %ROOT_PATH%\builds\win32\msvc6\*.plg 2>nul
 @del %ROOT_PATH%\builds\win32\msvc7\*.plg 2>nul
-
-@Echo Cleaning install files
-@del %ROOT_PATH%\src\install\arch-specific\win32\*.dll 2>nul
-@rmdir /s /q %ROOT_PATH%\builds\win32\install_image
+@del %ROOT_PATH%\builds\install\arch-specific\win32\*.dll 2>nul
+@rmdir /s /q %ROOT_PATH%\builds\win32\install_image 2>nul
