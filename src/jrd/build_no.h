ae464b4ef01e8b9ed69d6eec1cb97be9aa14afba
bellardo <bellardo@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-07-19 04:57:39 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@7007 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:4019 
+  FORMAL BUILD NUMBER:4020 
 */
 
-#define PRODUCT_VER_STRING "1.6.0.4019"
-#define FILE_VER_STRING "WI-T1.6.0.4019"
-#define LICENSE_VER_STRING "WI-T1.6.0.4019"
-#define FILE_VER_NUMBER 1, 6, 0, 4019
+#define PRODUCT_VER_STRING "1.6.0.4020"
+#define FILE_VER_STRING "WI-T1.6.0.4020"
+#define LICENSE_VER_STRING "WI-T1.6.0.4020"
+#define FILE_VER_NUMBER 1, 6, 0, 4020
 #define FB_MAJOR_VER "1"
 #define FB_MINOR_VER "6"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "4019"
+#define FB_BUILD_NO "4020"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 1.6 Alpha 1"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.4016 2003-07-19 04:53:18 bellardo Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.4017 2003-07-19 04:53:50 bellardo Exp $"
 
 BuildType=T
 MajorVer=1
 MinorVer=6
 RevNo=0
-BuildNum=4018
+BuildNum=4019
 BuildSuffix="Firebird 1.6 Alpha 1"
 FIREBIRD_PACKAGE_VERSION=0.Alpha1
 
@@ -82,7 +82,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=4018
+BuildNum=4019
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
