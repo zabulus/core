afa7f63a1ac3eca8fdc8ce65f53432ea61383a0f
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-08-04 22:33:56 +0000

    Fix B1_5_Release build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@7132 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,15 +3,15 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.3741.2.2 2003-07-17 08:42:28 dimitr Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.3741.2.3 2003-08-04 22:33:56 skidder Exp $"
 
 BuildType=V
 MajorVer=1
 MinorVer=5
 RevNo=0
-BuildNum=3744
-BuildSuffix="Firebird 1.5"
-FIREBIRD_PACKAGE_VERSION=Release
+BuildNum=3773
+BuildSuffix="Firebird 1.5 Release Candidate 5"
+FIREBIRD_PACKAGE_VERSION=RC5
 
 FIREBIRD_VERSION="$MajorVer.$MinorVer.$RevNo"
 PRODUCT_VER_STRING="$MajorVer.$MinorVer.$RevNo.$BuildNum"
