4dbe94357f2b7b583d014b54d2721b40e0a5a04c
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-08-09 18:49:09 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@7252 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:4150 
+  FORMAL BUILD NUMBER:4151 
 */
 
-#define PRODUCT_VER_STRING "2.0.0.4150"
-#define FILE_VER_STRING "WI-T2.0.0.4150"
-#define LICENSE_VER_STRING "WI-T2.0.0.4150"
-#define FILE_VER_NUMBER 2, 0, 0, 4150
+#define PRODUCT_VER_STRING "2.0.0.4151"
+#define FILE_VER_STRING "WI-T2.0.0.4151"
+#define LICENSE_VER_STRING "WI-T2.0.0.4151"
+#define FILE_VER_NUMBER 2, 0, 0, 4151
 #define FB_MAJOR_VER "2"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "4150"
+#define FB_BUILD_NO "4151"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 2.0 Alpha 1"
