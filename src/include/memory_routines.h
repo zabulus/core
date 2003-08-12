ecfce9ecf978df82c914e031eb27a720b2e5bab8
aafemt <aafemt@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-08-12 10:42:52 +0000

    Misc inline memory routine
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@7397 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

---
+++ src/include/memory_routines.h
@@ -0,0 +1,212 @@
+/*
+ *	PROGRAM:	Client/Server Common Code
+ *	MODULE:		memory_routines.h
+ *	DESCRIPTION:	Misc memory content routines
+ *
+ * The contents of this file are subject to the Interbase Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy
+ * of the License at http://www.Inprise.com/IPL.html
+ *
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code was created by Inprise Corporation
+ * and its predecessors. Portions created by Inprise Corporation are
+ * Copyright (C) Inprise Corporation.
+ *
+ * Created by: Dimitry Sibiryakov <aafemt@users.sourceforge.net>
+ *
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ */
+
+#ifndef MEMORY_ROUTINES_H
+#define MEMORY_ROUTINES_H
+
+/* SD: I didn't find a way to make external functions inline
+       except including their bodies into header. FIX ME, please */
+/*
+extern inline short get_short(const char*);
+extern inline long get_long(const char*);
+extern inline short gather_short(char* &);
+extern inline long gather_long(char* &);
+*/
+
+inline short get_short(const char* p)
+{
+/**************************************
+ *
+ *      g e t _ s h o r t
+ *
+ **************************************
+ *
+ * Functional description
+ *    gather one short int from two chars
+ *
+ * Based on BTR_get_quad
+ *
+ **************************************/
+
+#if defined(i386) || defined(I386) || defined(_M_IX86) || defined(VMS)
+	// For IA32 (little-endian) this optimization is a _very_ large speed-up!
+	// According to CTO32L definition in common.h this trick work for VAX/VMS
+	return *reinterpret_cast<const short*>(p);
+#else
+	// Non-IA32
+
+union {
+ short n;
+ char c[2]; } value;
+
+	value.c[0] = p[0];
+	value.c[1] = p[1];
+
+	return value.n;
+
+#endif	// endianness
+}
+
+inline long get_long(const char* p)
+{
+/**************************************
+ *
+ *      g e t _ l o n g
+ *
+ **************************************
+ *
+ * Functional description
+ *    gather one long int from four chars
+ *
+ * Based on BTR_get_quad
+ *
+ **************************************/
+
+#if defined(i386) || defined(I386) || defined(_M_IX86) || defined(VMS)
+	// For IA32 (little-endian) this optimization is a _very_ large speed-up!
+	return *reinterpret_cast<const long*>(p);
+#else
+	// Non-IA32
+
+union {
+ long n;
+ char c[4]; } value;
+
+	value.c[0] = p[0];
+	value.c[1] = p[1];
+	value.c[2] = p[2];
+	value.c[3] = p[3];
+
+	return value.n;
+
+#endif	// endianness
+}
+
+inline unsigned long get_long(const unsigned char* p)
+{
+/**************************************
+ *
+ *      g e t _ l o n g (overloaded)
+ *
+ **************************************
+ *
+ * Functional description
+ *    gather one unsigned long int from
+ *    four unsigned chars
+ *
+ * Based on get_long and CTO32L macro
+ *
+ **************************************/
+
+#if defined(i386) || defined(I386) || defined(_M_IX86) || defined(VMS)
+	// For IA32 (little-endian) this optimization is a _very_ large speed-up!
+	return *reinterpret_cast<const unsigned long*>(p);
+#else
+	// Non-IA32 a bit slower implementation but faster than using union
+#ifndef WORDS_BIGENDIAN
+	return (p[3] << 24) | (p[2] << 16) | (p[1] << 8) | p[0];
+#else
+	return (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
+#endif // WORDS_BIGENDIAN
+#endif
+}
+
+inline short gather_short(char* &p)
+{
+/**************************************
+ *
+ *      g a t h e r _ s h o r t
+ *
+ **************************************
+ *
+ * Functional description
+ *    gather one short int from two chars
+ *    and advance the pointer
+ *
+ * Based on BTR_get_quad
+ *
+ **************************************/
+
+#if defined(i386) || defined(I386) || defined(_M_IX86) || defined(VMS)
+	// For IA32 (little-endian) this optimization is a _very_ large speed-up!
+	short value;
+	value = *reinterpret_cast<const short*>(p);
+	p += 2;
+	return value;
+#else
+	// Non-IA32
+
+union {
+ short n;
+ char c[2]; } value;
+
+	value.c[0] = *p++;
+	value.c[1] = *p++;
+
+	return value.n;
+
+#endif	// endianness
+}
+
+inline long gather_long(char* &p)
+{
+/**************************************
+ *
+ *      g a t h e r _ l o n g
+ *
+ **************************************
+ *
+ * Functional description
+ *    gather one long int from four chars
+ *    and advance the pointer
+ *
+ * Based on BTR_get_quad
+ *
+ **************************************/
+
+#if defined(i386) || defined(I386) || defined(_M_IX86) || defined(VMS)
+	// For IA32 (little-endian) this optimization is a _very_ large speed-up!
+	long value;
+	value = *reinterpret_cast<const long*>(p);
+	p += 4;
+	return value;
+#else
+	// Non-IA32
+
+union {
+ long n;
+ char c[4]; } value;
+
+	value.c[0] = *p++;
+	value.c[1] = *p++;
+	value.c[2] = *p++;
+	value.c[3] = *p++;
+
+	return value.n;
+
+#endif	// endianness
+}
+
+#endif // MEMORY_ROUTINES_H
