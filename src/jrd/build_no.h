2430f73cd4ca56e85dff578b2a1864549d0ab0a5
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-09-02 18:19:45 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@7957 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:4545 
+  FORMAL BUILD NUMBER:4546 
 */
 
-#define PRODUCT_VER_STRING "2.0.0.4545"
-#define FILE_VER_STRING "WI-T2.0.0.4545"
-#define LICENSE_VER_STRING "WI-T2.0.0.4545"
-#define FILE_VER_NUMBER 2, 0, 0, 4545
+#define PRODUCT_VER_STRING "2.0.0.4546"
+#define FILE_VER_STRING "WI-T2.0.0.4546"
+#define LICENSE_VER_STRING "WI-T2.0.0.4546"
+#define FILE_VER_NUMBER 2, 0, 0, 4546
 #define FB_MAJOR_VER "2"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "4545"
+#define FB_BUILD_NO "4546"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 2.0 Alpha 1"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.4544 2003-09-02 17:57:38 brodsom Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.4545 2003-09-02 18:19:45 brodsom Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=4545
+BuildNum=4546
 BuildSuffix="Firebird 2.0 Alpha 1"
 FIREBIRD_PACKAGE_VERSION=0.Alpha1
 
@@ -82,7 +82,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=4545
+BuildNum=4546
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
