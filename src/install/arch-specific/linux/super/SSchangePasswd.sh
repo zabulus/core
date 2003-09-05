0c6854c961146b64ab6718c8f228309646296e03
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-09-05 10:54:16 +0000

    fix posix SS build - now rpm package appear to be successfully buildable
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@8045 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

---
+++ src/install/arch-specific/linux/super/SSchangePasswd.sh
@@ -0,0 +1,139 @@
+#! /bin/sh
+
+
+# This shell script changes both the SYSDBA user and the firebird 
+# install script.
+
+#------------------------------------------------------------------------
+# Prompt for response, store result in Answer
+
+Answer=""
+
+AskQuestion() {
+    Test=$1
+    DefaultAns=$2
+    echo -n "${1}"
+    Answer="$DefaultAns"
+    read Answer
+}
+
+
+#------------------------------------------------------------------------
+#  changeInitPassword
+
+
+changeInitPassword() {
+
+    NewPasswd=$1
+
+    InitFile=/etc/rc.d/init.d/firebird
+    if [ -f $InitFile ] 
+      then
+        ed $InitFile <<EOF
+/ISC_PASSWORD/s/ISC_PASSWORD:=.*\}/ISC_PASSWORD:=$NewPasswd\}/g
+w
+q
+EOF
+    chmod u=rwx,g=rx,o= $InitFile
+
+    fi
+}
+
+#------------------------------------------------------------------------
+#  Generate new sysdba password
+
+
+generateNewDBAPassword() {
+
+    DBAPasswordFile=$IBRootDir/SYSDBA.password
+    NewPasswd=`mkpasswd -l 8`
+    echo "Firebird generated password " > $DBAPasswordFile
+    echo "for user SYSDBA is : $NewPasswd" >> $DBAPasswordFile
+    echo "generated on `hostname` at time `date`" >> $DBAPasswordFile
+    echo "" >> $DBAPasswordFile
+    echo "(For superserver you will also want to check the password in the" >> $DBAPasswordFile
+    echo "daemon init routine in the file /etc/rc.d/init.d/firebird)" >> $DBAPasswordFile
+    echo "" >> $DBAPasswordFile
+    echo "Your password can be changed to a more suitable one using the" >> $DBAPasswordFile
+    echo "/usr/local/firebird/bin/gsec program as show below:" >> $DBAPasswordFile
+    echo "" >> $DBAPasswordFile
+    echo ">cd /usr/local/firebird" >> $DBAPasswordFile
+    echo ">bin/gsec -user sysdba -password <password>" >> $DBAPasswordFile
+    echo "GSEC>modify sysdba -pw <newpassword>" >> $DBAPasswordFile
+    echo "GSEC>quit" >> $DBAPasswordFile
+
+    chmod u=rw,go= $DBAPasswordFile
+
+    $IBBin/gsec -user sysdba -password masterkey <<EOF
+modify sysdba -pw $NewPasswd
+EOF
+
+    changeInitPassword "$NewPasswd"
+
+
+}
+
+
+#------------------------------------------------------------------------
+#  Change sysdba password - this routine is interactive and is only 
+#  used in the install shell script not the rpm one.
+
+
+askUserForNewDBAPassword() {
+
+    OrigPasswd=""
+    while [ -z "$OrigPasswd" ]
+      do
+          AskQuestion "Please enter current password for SYSDBA user: "
+          OrigPasswd=$Answer
+          
+      done
+
+    NewPasswd=""
+
+    while [ -z "$NewPasswd" ]
+      do
+          AskQuestion "Please enter new password for SYSDBA user: "
+          NewPasswd=$Answer
+          if [ ! -z "$NewPasswd" ]
+            then
+             $IBBin/gsec -user sysdba -password $OrigPasswd <<EOF
+modify sysdba -pw $NewPasswd
+EOF
+              echo ""
+
+              changeInitPassword "$NewPasswd"
+
+          fi
+          
+      done
+}
+
+
+#------------------------------------------------------------------------
+#  Change sysdba password - this routine is interactive and is only 
+#  used in the install shell script not the rpm one.
+
+
+changeDBAPassword() {
+
+    if [ -z "$InteractiveInstall" ]
+      then
+        generateNewDBAPassword
+      else
+        askUserForNewDBAPassword
+    fi
+}
+
+
+#= Main Post ===============================================================
+
+
+    IBRootDir=/usr/local/firebird
+    IBBin=$IBRootDir/bin
+    RunUser=firebird
+
+    InteractiveInstall=true
+    export InteractiveInstall
+
+    changeDBAPassword
