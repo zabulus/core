a7cd12cc49c0a3934873a88a95d1bc370f17b912
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-09-16 08:34:08 +0000

    Too trivial change
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@8460 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/utilities/gsec/gsec_proto.h
+++ src/utilities/gsec/gsec_proto.h
@@ -5,7 +5,7 @@
 extern int GSEC_main(SVC service);
 #endif
 
-/* Output reporting utilities */
+// Output reporting utilities
 extern void GSEC_print_status(ISC_STATUS *);
 extern void GSEC_error_redirect(ISC_STATUS *, USHORT, TEXT *, TEXT *);
 extern void GSEC_error(USHORT, TEXT *, TEXT *, TEXT *, TEXT *, TEXT *);
