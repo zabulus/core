ff663cfd4a1e7c3b6ace6f279278a927cae89791
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-10-09 03:54:26 +0000

    Fix script for SF bug #819224 => Change the comment, too.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@9092 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/extlib/fbudf/fbudf.sql
+++ src/extlib/fbudf/fbudf.sql
@@ -162,7 +162,7 @@ int by descriptor, int by descriptor
 returns parameter 2
 entry_point 'fbround' module_name 'fbudf';
 
---FBUDF_API paramdsc* round(paramdsc* v, paramdsc* rc)
+--FBUDF_API paramdsc* fbround(paramdsc* v, paramdsc* rc)
 declare external function i64Round
 numeric(18, 4) by descriptor, numeric(18, 4) by descriptor
 returns parameter 2
