eaa389966c6c2a7c70ae03d97b03943bc4239ce6
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-10-09 17:48:29 +0000

    Test if firebird is set and ask for it if not
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@9103 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -1,8 +1,38 @@
+
+@echo off
+
+@if "%1" NEQ "" (set FIREBIRD=%1)
+:: BRS 
+:: Get all the file name when there are spaces
+:: this can be also achieved with %* but I don't know which versions of 
+:: windows allows it
+:LOOP
+	@shift
+	@if "%1"==""  (goto DONE)
+        @set FIREBIRD=%FIREBIRD% %1
+	@goto loop
+:DONE
+
+@echo      FIREBIRD=%FIREBIRD%
+@if "%FIREBIRD%"=="" (goto :HELP & goto :EOF)
+
 call clean_all
-call prepare c:\opt\firebird_15
+call prepare
 call make_boot
 call make_all
 call make_examples
 cd ..\install\arch-specific\win32
 call BuildExecutableInstall
 cd ..\..\..\win32
+
+::===========
+:HELP
+@echo.
+@echo   Build process need the FIREBIRD environment variable set to work.
+@echo   FIREBIRD value should be the root directory of your Firebird installation.
+@echo   Example:
+@echo     c:\program files\firebird
+@echo. 
+@goto :END
+
+:END
