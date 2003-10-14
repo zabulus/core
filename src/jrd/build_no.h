ad1b1f3a8908dcae1068d10a60a40b7b19b80770
eku <eku@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-10-14 13:10:06 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@9175 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:5232 
+  FORMAL BUILD NUMBER:5233 
 */
 
-#define PRODUCT_VER_STRING "2.0.0.5232"
-#define FILE_VER_STRING "WI-T2.0.0.5232"
-#define LICENSE_VER_STRING "WI-T2.0.0.5232"
-#define FILE_VER_NUMBER 2, 0, 0, 5232
+#define PRODUCT_VER_STRING "2.0.0.5233"
+#define FILE_VER_STRING "WI-T2.0.0.5233"
+#define LICENSE_VER_STRING "WI-T2.0.0.5233"
+#define FILE_VER_NUMBER 2, 0, 0, 5233
 #define FB_MAJOR_VER "2"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "5232"
+#define FB_BUILD_NO "5233"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 2.0 Alpha 1"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.5230 2003-10-14 13:10:02 eku Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.5231 2003-10-14 13:10:04 eku Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=5231
+BuildNum=5232
 BuildSuffix="Firebird 2.0 Alpha 1"
 FIREBIRD_PACKAGE_VERSION=0.Alpha1
 
@@ -82,7 +82,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=5231
+BuildNum=5232
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
