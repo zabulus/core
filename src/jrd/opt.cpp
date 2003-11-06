76745a5edf11a310d86b35c9d1ce4c71d5edd291
arnobrinkman <arnobrinkman@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-11-06 00:21:05 +0000

    bugfix: A user specified PLAN with MERGE and JOINS crashed the server, noted by Dmitry.   simplified : PLAN MERGE (JOIN ..., JOIN ...)
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@9696 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/opt.cpp
+++ src/jrd/opt.cpp
@@ -3188,15 +3188,19 @@ static void form_rivers(TDBB tdbb,
 		temp[temp[0]] = (UCHAR) relation_node->nod_arg[e_rel_stream];
 	}
 
-/* just because the user specified a join does not mean that 
-   we are able to form a river;  thus form as many rivers out
-   of the join are as necessary to exhaust the streams */
-
-	do
-		count = find_order(tdbb, opt, temp, plan_node);
-	while (form_river
-		   (tdbb, opt, count, streams, temp, river_stack, sort_clause,
-			project_clause, 0));
+	// just because the user specified a join does not mean that 
+	// we are able to form a river;  thus form as many rivers out
+	// of the join are as necessary to exhaust the streams 
+	// AB: Only form rivers when any retrieval node is seen, for
+	// example a MERGE on two JOINs will come with no retrievals
+	// at this point.
+	if (temp[0] != 0) {
+		do
+			count = find_order(tdbb, opt, temp, plan_node);
+		while (form_river
+			   (tdbb, opt, count, streams, temp, river_stack, sort_clause,
+				project_clause, 0));
+	}
 }
 
 
