43e4259145b19a773b5e97ccca5979223435ae0d
bellardo <bellardo@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-11-09 22:53:12 +0000

    The rest of the pthread_mutex spinlock implementation
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@9877 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/locks.h
+++ src/common/classes/locks.h
@@ -100,6 +100,7 @@ public:
 	}
 };
 #else
+#ifdef SOLARIS
 // Who knows why Solaris 2.6 have not THIS funny spins?
 //The next code is not comlpeted but let me compile //Konstantin
 class Spinlock {
@@ -123,6 +124,29 @@ public:
 			system_call_failed::raise();
 	}
 };
+#else  // DARWIN
+class Spinlock {
+private:
+	pthread_mutex_t mlock;
+public:
+	Spinlock() {
+		if (pthread_mutex_init(&mlock, 0))
+			system_call_failed::raise();
+	}
+	~Spinlock() {
+		if (pthread_mutex_destroy(&mlock))
+			system_call_failed::raise();
+	}
+	void enter() {
+		if (pthread_mutex_lock(&mlock))
+			system_call_failed::raise();
+	}
+	void leave() {
+		if (pthread_mutex_unlock(&mlock))
+			system_call_failed::raise();
+	}
+};
+#endif
 
 #endif
 #endif
