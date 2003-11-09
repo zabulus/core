7f72c8e38f3c306a393f4ab180486fc23a0f9aba
bellardo <bellardo@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-11-09 19:58:26 +0000

    use PowerPC instead of DARWIN
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@9869 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/remote/xdr.cpp
+++ src/remote/xdr.cpp
@@ -42,7 +42,7 @@
 #if defined(i386) || defined(I386) || defined(_M_IX86)
 #define		SWAP_DOUBLE
 #else
-#if !defined(sparc) && !defined(DARWIN)
+#if !defined(sparc) && !defined(PowerPC)
 #error "Define SWAP_DOUBLE for your platform correctly !"
 #endif
 #endif
