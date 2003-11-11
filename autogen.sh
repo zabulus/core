049dc8c02cd81df6718915baa76c722dece55c2f
tmcsys <tmcsys@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-11-11 08:24:11 +0000

    1) User can specify tool path in ENV for systems with multiple versions of tools installed.  For example:
    /bin/sh
    AUTOCONF=[path_to_autoconf]/autoconf; export AUTOCONF
    LIBTOOL=[path_to_libtool]/libtool; export LIBTOOL
    LIBTOOLIZE=[path_to_libtooliize]/libtoolize; export LIBTOOLIZE
    ./autogen.sh
    
    
    2) Autoconf version check.  Obsolete versions were caught by AC_PREREQ in
    configure.in and not by this script.
    
    Libtool version check may also pass obsolete versions.
    
    
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@9956 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- autogen.sh
+++ autogen.sh
@@ -2,16 +2,33 @@
 #
 # Run this to generate all the initial makefiles, etc.
 #
-# $Id: autogen.sh,v 1.8 2003-04-17 18:41:28 bellardo Exp $
+# $Id: autogen.sh,v 1.8.2.1 2003-11-11 08:24:11 tmcsys Exp $
 
 PKG_NAME=Firebird2
 SRCDIR=`dirname $0`
 DIE=0
 
-VER=`autoconf --version|grep '^[Aa]utoconf'|sed 's/^[^0-9]*//'`
+if [ -z $AUTOCONF ]
+then
+  AUTOCONF=autoconf
+fi
+if [ -z $LIBTOOL ]
+then
+  LIBTOOL=libtool
+fi
+if [ -z $LIBTOOLIZE ]
+then
+  LIBTOOLIZE=libtoolize
+fi
+
+echo "AUTOCONF="$AUTOCONF
+echo "LIBTOOL="$LIBTOOL
+echo "LIBTOOLiZE="$LIBTOOLIZE
+
+VER=`$AUTOCONF --version|grep '^[Aa]utoconf'|sed 's/^[^0-9]*//'`
 case "$VER" in
  0* | 1\.* | 2\.[0-9] | 2\.[0-9][a-z]* | \
- 2\.[1-4][0-9] | 2\.5[0-2][a-z]* )
+ 2\.[1-4][0-9] | 2\.5[0-2]* )
   echo
   echo "**Error**: You must have autoconf 2.53 or later installed."
   echo "Download the appropriate package for your distribution/OS,"
@@ -20,7 +37,7 @@ case "$VER" in
   ;;
 esac
 
-VER=`libtool --version|grep ' libtool)'|sed 's/.*) \([0-9][0-9.]*\) .*/\1/'`
+VER=`$LIBTOOL --version|grep ' libtool)'|sed 's/.*) \([0-9][0-9.]*\) .*/\1/'`
 case "$VER" in
  0* | 1\.[0-2] | 1\.[0-2][a-z]* | \
  1\.3\.[0-2] | 1\.3\.[0-2][a-z]* )
@@ -33,7 +50,6 @@ case "$VER" in
 esac
 
 # Put other tests for programs here!
-
 
 # If anything failed, exit now.
 if test "$DIE" -eq 1; then
@@ -50,15 +66,15 @@ fi
 
 # Generate configure from configure.in
 echo "Running libtoolize ..."
-LIBTOOL_M4=`libtoolize --copy --force --dry-run|grep 'You should add the contents of'|sed "s,^[^/]*\(/[^']*\).*$,\1,"`
+LIBTOOL_M4=`$LIBTOOLIZE --copy --force --dry-run|grep 'You should add the contents of'|sed "s,^[^/]*\(/[^']*\).*$,\1,"`
 if test "x$LIBTOOL_M4" != "x"; then
  rm -f aclocal.m4
  cp $LIBTOOL_M4 aclocal.m4
 fi
-libtoolize --copy --force || exit 1
+$LIBTOOLIZE --copy --force || exit 1
 
 echo "Running autoconf ..."
-autoconf || exit 1
+$AUTOCONF || exit 1
 
 # If NOCONFIGURE is set, skip the call to configure
 if test "x$NOCONFIGURE" = "x"; then
