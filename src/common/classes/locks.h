ab2b1c0dc7d4807f50a3e9f336f223dab7669c7b
stryqx <stryqx@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-11-11 23:57:00 +0000

    Fix the spinlocks case for FreeBSD 4.x
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@9993 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/locks.h
+++ src/common/classes/locks.h
@@ -77,7 +77,7 @@ public:
 
 /* Process-local spinlock. Used to manage memory heaps in threaded environment. */
 // Pthreads version of the class
-#if !defined(SOLARIS) && !defined(DARWIN)
+#if !defined(SOLARIS) && !defined(DARWIN) && !defined(FREEBSD)
 class Spinlock {
 private:
 	pthread_spinlock_t spinlock;
@@ -124,7 +124,7 @@ public:
 			system_call_failed::raise();
 	}
 };
-#else  // DARWIN
+#else  // DARWIN and FREEBSD
 class Spinlock {
 private:
 	pthread_mutex_t mlock;
