8ffc9cb5b6d1a384ad264edda645d59801a5c82d
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-12-02 16:04:27 +0000

    Fix posix build (const_cast needed)
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@10415 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/iscguard/iscguard.cpp
+++ src/iscguard/iscguard.cpp
@@ -138,7 +138,7 @@ int WINAPI WinMain(
 /* since the flag is set we run as a service */
 	if (service_flag == true) {
 		CNTL_init((FPTR_VOID) WINDOW_main, ISCGUARD_SERVICE);
-		if (!StartServiceCtrlDispatcher(service_table)) {
+		if (!StartServiceCtrlDispatcher(const_cast<SERVICE_TABLE_ENTRY*>(service_table))) {
 			if (GetLastError() != ERROR_CALL_NOT_IMPLEMENTED)
 				CNTL_shutdown_service("StartServiceCtrlDispatcher failed");
 		}
