b2bc14ea8f2a1291a72117c4f36d99a6dbd21712
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-12-02 13:40:06 +0000

    Fixed use of descending index as table's primary key. Problems were related with referencing such table in foreign key.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@10407 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/idx.cpp
+++ src/jrd/idx.cpp
@@ -1142,8 +1142,14 @@ JRD_REL partner_relation, SSHORT index_id)
 		retrieval.irb_generic = irb_equality;
 		retrieval.irb_relation = partner_relation;
 		retrieval.irb_key = &key;
-		retrieval.irb_upper_count = idx->idx_count;
-		retrieval.irb_lower_count = idx->idx_count;
+		if (partner_idx.idx_flags & idx_descending) {
+			retrieval.irb_generic |= irb_descending;
+			retrieval.irb_upper_count = retrieval.irb_lower_count = 0;
+		}
+		else {
+			retrieval.irb_upper_count = 
+				retrieval.irb_lower_count = idx->idx_count;
+		}
 		BTR_evaluate(tdbb, &retrieval, &bitmap);
 
 		/* if there is a bitmap, it means duplicates were found */
