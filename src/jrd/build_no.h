40ff8c7af23ec2ab0b5ad48c6be1e58b0f228c66
skywalker <skywalker@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-12-15 04:32:52 +0000

    Update makefile for gstat add -lfbstatic to allow build to find db_alias.o
    Rollback xinetd flags - remove KEEPALIVE as Nickolay has this option hardcoded
    and it is not required.
    
    Update make clean to remove links as well
    
    Change troubleshooting doco to use /opt/firebird rather than /usr/local/firebird
    
    Add fb_lock_print to super install package
    
    Fix (my) syntax error in super uninstall
    
    Change super install scripts to use FBXXXX rather than IBXXX, I kept getting
    into trouble copying stuff from classic to super - now they are both the same
    
    also add a fb_config utility (undocumented currently)
    
    
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@10591 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,17 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER: 4027
+  FORMAL BUILD NUMBER:4075 
 */
 
 #define PRODUCT_VER_STRING "1.5.0.4075"
-#define FILE_VER_STRING "WI-T1.5.0.4075"
-#define LICENSE_VER_STRING "WI-T1.5.0.4075"
+#define FILE_VER_STRING "WI-V1.5.0.4075"
+#define LICENSE_VER_STRING "WI-V1.5.0.4075"
 #define FILE_VER_NUMBER 1, 5, 0, 4075
 #define FB_MAJOR_VER "1"
 #define FB_MINOR_VER "5"
 #define FB_REV_NO "0"
 #define FB_BUILD_NO "4075"
-
-#define FB_BUILD_TYPE "T"
+#define FB_BUILD_TYPE "V"
 #define FB_BUILD_SUFFIX "Firebird 1.5 Release Candidate 7"
