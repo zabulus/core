c5d0b9bdd6fcf6e094b4f0aa50e0634cd407340a
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2003-12-24 15:09:23 +0000

    Fixed one more server crash.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@10716 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/ipserver/ipserver.cpp
+++ src/ipserver/ipserver.cpp
@@ -1282,6 +1282,7 @@ static void end_blob( ICC icc, P_OP operation)
  **************************************/
 	IBL blob;
 	ISC_STATUS_ARRAY status_vector;
+	ISC_STATUS result;
 	UCHAR *buffer;
 	USHORT length;
 	ips_object *ips;
@@ -1313,11 +1314,12 @@ static void end_blob( ICC icc, P_OP operation)
 	// perform operation, send response and free resources 
 
 	if (operation == op_close_blob)
-		GDS_CLOSE_BLOB(status_vector, &blob->ibl_handle);
+		result = GDS_CLOSE_BLOB(status_vector, &blob->ibl_handle);
 	else
-		GDS_CANCEL_BLOB(status_vector, &blob->ibl_handle);
+		result = GDS_CANCEL_BLOB(status_vector, &blob->ibl_handle);
 	send_response(icc, status_vector);
-	release_blob(blob);
+	if (!result)
+		release_blob(blob);
 }
 
 
