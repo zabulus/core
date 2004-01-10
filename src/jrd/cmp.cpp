b0d96a809a275523eb97a6ec390c05f6e8d9bce0
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-01-10 15:15:52 +0000

    Fixed bug with computed fields referenced in a trigger context.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B1_5_Release@10958 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/cmp.cpp
+++ src/jrd/cmp.cpp
@@ -3145,11 +3145,9 @@ static JRD_NOD pass1(
 			 */
 
 			if (tail->csb_flags & (csb_view_update | csb_trigger)) {
-			// dimitr:	this code is rollbacked to its previous state
-			//			to allow computed fields in views to be updatable
-			//			via triggers
-			//												2003.11.05
-			//	if (!(field->fld_computation))
+				// dimitr:	added an extra check for views, because we don't
+				//			want their old/new contexts to be substituted
+				if (relation->rel_view_rse || !field->fld_computation)
 					break;
 			}
 
