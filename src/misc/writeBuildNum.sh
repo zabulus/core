cf89c16dae87f7a2a743d93dd7c9f1864ffb8973
aafemt <aafemt@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-04-06 05:53:38 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@12174 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.6986 2004-04-06 05:47:15 skidder Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.6987 2004-04-06 05:53:38 aafemt Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=6984
+BuildNum=6985
 BuildSuffix="Firebird 2.0 UNSTABLE"
 FIREBIRD_PACKAGE_VERSION=0.UNSTABLE
 
@@ -82,7 +82,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=6984
+BuildNum=6985
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
