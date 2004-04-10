eb8a73530bc86afc8a124df3fd00513fd716cc30
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-04-10 19:41:25 +0000

    Small fixes in Trace API header
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@12263 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/ntrace.h
+++ src/jrd/ntrace.h
@@ -32,14 +32,14 @@
  *  Contributor(s):
  * 
  *
- *  $Id: ntrace.h,v 1.1 2004-04-04 07:40:58 skidder Exp $
+ *  $Id: ntrace.h,v 1.2 2004-04-10 19:41:25 skidder Exp $
  *
  */
 
 /* Keep this header written using C language, please */
 
 
-/* Version of API, used for version fields in TraceDatabase and TracePlugin structures */
+/* Version of API, used for version fields in TracePlugin structure */
 #define NTRACE_VERSION 1
 
 #define NTRACE_TRUE 1
@@ -91,11 +91,14 @@ struct PerformanceInfo {
 	ntrace_counter_t pin_fetches; /* Page fetches from OS */
 
 	ntrace_size_t pin_count; /* Number of relations involved in analysis */
-	struct TableInfo *pin_tables; /* Pointer to array with table stats */
+	struct TableStats *pin_tables; /* Pointer to array with table stats */
 };
 
 /* Get error string for hook failure that happened */
 typedef char* (*ntrace_get_error)(ntrace_object_t tpl_object);
+
+/* Finalize plugin interface for this particular database */
+typedef void (*ntrace_finalize)(ntrace_object_t tpl_object);
 
 /* Create/close attachment */
 typedef ntrace_boolean_t (*ntrace_event_attach)(ntrace_object_t tpl_object, 
@@ -138,8 +141,12 @@ struct TracePlugin {
 	/* Object pointer to pass to each hook */
 	ntrace_object_t tpl_object;
 
+	/* Finalize plugin. Called when database is closed by the engine */
+	ntrace_finalize tpl_finalize;
+
 	/* Function to return error string for hook failure */
 	ntrace_get_error tpl_get_error;
+
 
 	/* Events supported */
 	ntrace_event_attach tpl_event_attach;
