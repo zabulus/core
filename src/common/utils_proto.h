2e7e4667d361cd5c296465dc0b305560b6f15180
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-07-10 03:17:02 +0000

    Preach with the example and change the license to the IDPL
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@13830 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/utils_proto.h
+++ src/common/utils_proto.h
@@ -1,30 +1,26 @@
 /*
- *  The contents of this file are subject to the Mozilla Public
- *  License Version 1.1 (the "License"); you may not use this file
- *  except in compliance with the License. You may obtain a copy of
- *  the License at http://www.mozilla.org/MPL/
- *  Alternatively, the contents of this file may be used under the
- *  terms of the GNU General Public License Version 2 or later (the
- *  "GPL"), in which case the provisions of the GPL are applicable
- *  instead of those above. You may obtain a copy of the Licence at
- *  http://www.gnu.org/copyleft/gpl.html
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  Relevant for more details.
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
  *
- *  This file was created by members of the firebird development team.
- *  All individual contributions remain the Copyright (C) of those
- *  individuals.  Contributors to this file are either listed here or
- *  can be obtained from a CVS history command.
+ *  The Original Code was created by Claudio Valderrama on 25-Dec-2003
+ *  for the Firebird Open Source RDBMS project.
  *
- *  All rights reserved.
- *  Claudio Valderrama created this file on 25-Dec-2003.
+ *  Copyright (c) 2003 Claudio Valderrama
+ *  and all contributors signed below.
  *
- *  Contributor(s):
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
  *
  */
+
 
 // =====================================
 // Utility functions
