f33178c586fcacf7e89779b2b7a228fd41ff78d2
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-08-20 05:11:55 +0000

    Erik's script producing source bundle including configure script. Invoked via: make srctarfile
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14128 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/src_bundle.sh
+++ src/misc/src_bundle.sh
@@ -1,6 +1,47 @@
-FB_VERSION=1.5.1.4481
-FB_REVISION=R1_5_1
-cvs -z9 -d :ext:skidder@cvs.firebird.sourceforge.net:/cvsroot/firebird co -r $FB_REVISION firebird2
-mv firebird2 firebird-$FB_VERSION
-find firebird-$FB_VERSION -name CVS -exec rm -rf '{}' ';'
-tar -cjf firebird-$FB_VERSION.tar.bz2 firebird-$FB_VERSION
+#/bin/sh
+#
+# Script to checkout a clean copy of the sourcecode from CVS
+# and prepare it as an official "source distribution"
+#
+
+# Determine root of local checkout
+SRCROOT=`dirname $0`/../..
+
+# Set the CVS root from the local checkout if possible
+ROOT=`cat $SRCROOT/CVS/Root`
+if [ "x$ROOT" == 'x' ]; then
+    ROOT=":pserver:anonymous@cvs.sourceforge.net:/cvsroot/firebird"
+fi
+
+# Set the tag or branch based on the current checkout, if possible
+CVSTAG='$Name: not supported by cvs2svn $'
+TAG=`echo $CVSTAG | sed -e 's/\\$Name: not supported by cvs2svn $/\1/' | sed -e 's/ //'`
+if [ "x$TAG" == 'x' ]; then
+    TAG="HEAD"
+fi
+
+MODULE=firebird2
+NAME=firebird
+rm -rf $MODULE
+
+echo "Checking out $MODULE from $ROOT using $TAG"
+cvs -z9 -d $ROOT export -r $TAG $MODULE
+
+# Load version information from the new checkout
+source $MODULE/src/misc/writeBuildNum.sh
+DIRNAME="$NAME-$PRODUCT_VER_STRING"
+
+echo "Creating tarball for $DIRNAME"
+rm -rf $DIRNAME
+mv $MODULE $DIRNAME
+cd $DIRNAME
+
+echo "Generating configure script"
+NOCONFIGURE=1 . ./autogen.sh > /dev/null
+rm -Rf autom4te.cache
+rm -f aclocal.m4
+cd ..
+tar -cjf $DIRNAME.tar.bz2 $DIRNAME
+echo "New tarball is $DIRNAME.tar.bz2"
+rm -rf $DIRNAME
+
