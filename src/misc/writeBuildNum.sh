7052c1c9986b12bf8827d6b860eab9956fe9c25f
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-08-21 09:30:58 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14161 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.8122 2004-08-21 09:29:49 robocop Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.8123 2004-08-21 09:30:57 robocop Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=8120
+BuildNum=8121
 BuildSuffix="Firebird 2.0 UNSTABLE"
 FIREBIRD_PACKAGE_VERSION=0.UNSTABLE
 
@@ -82,7 +82,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=8120
+BuildNum=8121
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
