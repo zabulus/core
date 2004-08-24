2dfa79fc489f76835a191e0654a1de44cf730185
brodsom <brodsom@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-08-24 23:00:17 +0000

    Avoid final comma
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14224 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/qli/compile.h
+++ src/qli/compile.h
@@ -108,9 +108,9 @@ enum set_t {
     set_password,
     set_euc_justify,			// JPN specific option 
     set_count,
-    set_charset,
+    set_charset
 #ifdef DEV_BUILD
-    set_hex_output,
+    ,set_hex_output,
     set_explain
 #endif
 };
