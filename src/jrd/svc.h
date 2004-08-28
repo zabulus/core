acd0388f727f1c46e24374da981ae70c1323a595
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-08-28 02:55:45 +0000

    Change style of comment to avoid multi-line comment warning
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14319 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/svc.h
+++ src/jrd/svc.h
@@ -80,15 +80,16 @@ const USHORT isc_action_max				= 14;
 /* Macro used to store services thread specific data */
 /* Currently we store empty string, see bug #10394 */
 // BRS 01/07/2004 commented
-//#ifdef SUPERSERVER
-//#define SVC_PUTSPECIFIC_DATA	{
-//				char    t_data[] = {'\0'};
-//				thdd::putSpecificData((void*)t_data);
-//				}
-//#else
-//#define SVC_PUTSPECIFIC_DATA	/* nothing */
-//#endif
-
+/*
+#ifdef SUPERSERVER
+#define SVC_PUTSPECIFIC_DATA	{\
+				char    t_data[] = {'\0'};\
+				thdd::putSpecificData((void*)t_data);\
+				}
+#else
+#define SVC_PUTSPECIFIC_DATA	// nothing
+#endif
+*/
 
 struct serv_entry; // forward decl.
 
