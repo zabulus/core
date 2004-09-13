b77fc61733fbbe9fd8b10b9efcbe08ea9c244ad9
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-09-13 05:13:05 +0000

    Fix error in my code spotted by Claudio
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14488 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/val.h
+++ src/jrd/val.h
@@ -198,8 +198,10 @@ class ArrayField : public pool_alloc_rpt<internal_array_desc::iad_repeat, type_a
 	jrd_req*			arr_request;		/* request */
 	SLONG				arr_effective_length;	/* Length of array instance */
 	USHORT				arr_desc_length;	/* Length of array descriptor */
-	internal_array_desc	arr_desc;			/* Array descriptor */
 	ULONG				arr_temp_id;		// Temporary ID for open array inside the transaction
+
+	// Keep this field last as it is C-style open array !
+	internal_array_desc	arr_desc;			/* Array descriptor. ! */
 };
 
 #endif /* REQUESTER */
