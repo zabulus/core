b104b509e75508eb1c9b41117c001e28b9bfa2b6
kkuznetsov <kkuznetsov@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-09-15 08:31:32 +0000

    os/sun/server_main_dummy.cpp is a file with noop routine 	server_main to put into libfbclient 	if it defined by firebird.vers mapfile while 	linking libfbclient with Sun-like LD
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14539 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

---
+++ src/remote/os/sun/server_main_dummy.cpp
@@ -0,0 +1,15 @@
+#include "firebird.h"
+#include "../jrd/common.h"
+
+#ifndef MINGW
+
+extern "C" {
+int CLIB_ROUTINE server_main( int argc, char** argv){
+/*NOOP: this routine is neaded for implement
+	server_main as it defined by firebird.vers mapfile
+	linking libfbclient with Sun-like LD
+*/
+};
+}
+
+#endif
