10c3c726da531d87ad05fabaf8b29da28700ea3f
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-09-16 22:25:22 +0000

    Cleanup
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14548 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/remote/os/sun/server_main_dummy.cpp
+++ src/remote/os/sun/server_main_dummy.cpp
@@ -1,15 +1,12 @@
 #include "firebird.h"
 #include "../jrd/common.h"
 
-#ifndef MINGW
-
 extern "C" {
 int CLIB_ROUTINE server_main( int argc, char** argv){
 /*NOOP: this routine is neaded for implement
 	server_main as it defined by firebird.vers mapfile
 	linking libfbclient with Sun-like LD
 */
+	return 0;
 };
 }
-
-#endif
