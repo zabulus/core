c20aa6d7f073c1946491b68586974afa428091dc
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-09-22 11:07:51 +0000

    Misc
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@14635 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/remote/os/sun/server_main_dummy.cpp
+++ src/remote/os/sun/server_main_dummy.cpp
@@ -2,11 +2,12 @@
 #include "../jrd/common.h"
 
 extern "C" {
-int CLIB_ROUTINE server_main( int argc, char** argv){
-/*NOOP: this routine is neaded for implement
-	server_main as it defined by firebird.vers mapfile
+int CLIB_ROUTINE server_main( int argc, char** argv)
+{
+/*NOOP: this routine is needed to implement
+	server_main as it's defined by firebird.vers mapfile
 	linking libfbclient with Sun-like LD
 */
 	return 0;
-};
+}
 }
