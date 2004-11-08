162c242675afd82f79409bd08ecd344bfb051975
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-11-08 06:15:49 +0000

    Fix Alex' commit.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@16004 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/os/vms/vms.cpp
+++ src/jrd/os/vms/vms.cpp
@@ -257,7 +257,8 @@ jrd_file* PIO_create(Database* dbb, const TEXT* string, SSHORT length, bool over
 }
 
 
-int PIO_expand(const TEXT* file_name, USHORT file_length, TEXT* expanded_name, size_t len_expanded)
+int PIO_expand(const TEXT* file_name, USHORT file_length, TEXT* expanded_name,
+	size_t len_expanded)
 {
 /**************************************
  *
@@ -272,7 +273,7 @@ int PIO_expand(const TEXT* file_name, USHORT file_length, TEXT* expanded_name, s
  **************************************/
 	TEXT temp[NAM$C_MAXRSS], temp2[NAM$C_MAXRSS];
 
-	int length = ISC_expand_logical(file_name, file_length, expanded_name, bufsize);
+	int length = ISC_expand_logical(file_name, file_length, expanded_name, len_expanded);
 
 	struct FAB fab = cc$rms_fab;
 	struct NAM nam = cc$rms_nam;
@@ -282,14 +283,14 @@ int PIO_expand(const TEXT* file_name, USHORT file_length, TEXT* expanded_name, s
 	nam.nam$l_rsa = temp2;
 	nam.nam$b_rss = sizeof(temp2);
 	fab.fab$l_fna = expanded_name;
-	fab.fab$b_fns = bufsize; // length;
+	fab.fab$b_fns = len_expanded; // length;
 	fab.fab$l_dna = DEFAULT_FILE_NAME;
 	fab.fab$b_dns = sizeof(DEFAULT_FILE_NAME) - 1;
 
 	if ((sys$parse(&fab) & 1) && (sys$search(&fab) & 1)) {
 		length = nam.nam$b_rsl;
-		if (length >= bufsize)
-		    length = bufsize - 1;
+		if (length >= len_expanded)
+		    length = len_expanded - 1;
 		    
 		MOVE_FAST(temp2, expanded_name, length);
 		expanded_name[length] = 0;
