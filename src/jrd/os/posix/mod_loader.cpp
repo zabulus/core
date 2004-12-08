676616b44d1847925d74c2fe2a9379e87856bfe9
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-12-08 04:14:05 +0000

    Putting the license on John's files after asking him for permission.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@16896 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/os/posix/mod_loader.cpp
+++ src/jrd/os/posix/mod_loader.cpp
@@ -1,5 +1,27 @@
 /*
- *  mod_loader.cpp
+ *	PROGRAM:		JRD Module Loader
+ *	MODULE:			mod_loader.cpp
+ *	DESCRIPTION:	POSIX-specific class for loadable modules.
+ *
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by John Bellardo
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2002 John Bellardo <bellardo at cs.ucsd.edu>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
  *
  */
 
--- src/jrd/os/posix/path_utils.cpp
+++ src/jrd/os/posix/path_utils.cpp
@@ -1,3 +1,30 @@
+/*
+ *	PROGRAM:		JRD FileSystem Path Handler
+ *	MODULE:			path_utils.cpp
+ *	DESCRIPTION:	POSIX_specific class for file path management
+ *
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by John Bellardo
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2002 John Bellardo <bellardo at cs.ucsd.edu>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ *
+ */
+
 #include "../jrd/os/path_utils.h"
 #include <sys/types.h>
 #include <sys/stat.h>
