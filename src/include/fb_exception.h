a25394db35f3c4351408975c5ae899fc29370e9d
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-12-12 01:55:57 +0000

    Change the license with Mike's permission.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@17019 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/include/fb_exception.h
+++ src/include/fb_exception.h
@@ -1,25 +1,32 @@
-//
-//	Copyright (c) 2001 M. Nordell
-//
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ *	PROGRAM:		Firebird exceptions classes
+ *	MODULE:			fb_exception.h
+ *	DESCRIPTION:	Specific exception classes derived from std::exception
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Mike Nordell
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2001 Mike Nordell <tamlin at algonet.se>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ *
+ *
  */
-//
-// Special permission is given to the Firebird project to include,
-// use and modify this file in the Firebird database engine.
-//
+
+
 #ifndef FB_EXCEPTION_H
 #define FB_EXCEPTION_H
 
