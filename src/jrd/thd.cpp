d896c56c48af99ef50b63c4d9a9845d2150f6ca4
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2004-12-19 12:26:46 +0000

    fixed inconsistencies, noticed by Claudio
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@17145 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/thd.cpp
+++ src/jrd/thd.cpp
@@ -659,10 +659,9 @@ private:
 	Arg arg;
 public:
 	ThreadArgs(Routine r, Arg a) : routine(r), arg(a) { }
-	ThreadArgs(const ThreadArgs* t) : routine(t->routine), arg(t->arg) { }
+	ThreadArgs(const ThreadArgs& t) : routine(t.routine), arg(t.arg) { }
 	void run() {routine(arg);}
 private:
-	operator ThreadArgs();
 	ThreadArgs& operator=(const ThreadArgs&);
 };
 
@@ -670,8 +669,8 @@ THREAD_ENTRY_DECLARE threadStart(THREAD_ENTRY_PARAM arg) {
 	fb_assert(arg);
 	Firebird::ContextPoolHolder mainThreadContext(getDefaultMemoryPool());
 	{
-		ThreadArgs localArgs(reinterpret_cast<ThreadArgs*>(arg));
-		delete arg;
+		ThreadArgs localArgs(*reinterpret_cast<ThreadArgs*>(arg));
+		delete reinterpret_cast<ThreadArgs*>(arg);
 		localArgs.run();
 		return 0;
 	}
