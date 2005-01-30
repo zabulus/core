0b4d432345aecfb00a69c6612aecdafebe33e5d4
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-01-30 11:44:59 +0000

    license + misc
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@17694 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/string_test.cpp
+++ src/common/classes/string_test.cpp
@@ -5,6 +5,32 @@ char lbl[] = "0123456789";
 //If you plan to check range exception, you may uncomment it - 
 //anyway file test should not happen
 
+/*
+ *	PROGRAM:	Class library integrity tests
+ *	MODULE:		string_test.cpp
+ *	DESCRIPTION:	test class Firebird::string
+ *
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Alexander Peshkoff
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2004 Alexander Peshkoff <peshkoff@mail.ru>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ */
+
 #if defined(FIRESTR) && defined(DEV_BUILD)
 #define FULL_FIRE
 #endif
