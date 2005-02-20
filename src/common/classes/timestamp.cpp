27beeb00ce41ec24cf09cb66f0815dda5228695f
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-02-20 08:35:35 +0000

    no message
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@17966 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/timestamp.cpp
+++ src/common/classes/timestamp.cpp
@@ -111,7 +111,8 @@ void TimeStamp::decode_date(ISC_DATE nday, struct tm* times)
 	// zero-initialize them.
 	memset(times, 0, sizeof(struct tm));
 
-	const ISC_DATE origin = nday;
+	if ((times->tm_wday = (nday + 3) % 7) < 0)
+		times->tm_wday += 7;
 
 	nday -= 1721119 - 2400001;
 	const SLONG century = (4 * nday - 1) / 146097;
@@ -140,9 +141,6 @@ void TimeStamp::decode_date(ISC_DATE nday, struct tm* times)
 	times->tm_year = (int) year - 1900;
 
 	times->tm_yday = yday(times);
-
-	if ((times->tm_wday = (origin + 3) % 7) < 0)
-		times->tm_wday += 7;
 }
 
 
