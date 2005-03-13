e704f2b54a2fe702d4b560b1a82653ceac3646eb
arnobrinkman <arnobrinkman@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-03-13 12:17:32 +0000

    Fix typos found by Tom Cole
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@18140 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/dsql/make.cpp
+++ src/dsql/make.cpp
@@ -1466,7 +1466,7 @@ void MAKE_desc_from_list(dsc* desc, dsql_nod* node,
 			}
 
 			any_blob = true;
-			if (desc1.dsc_sub_type = 1) {
+			if (desc1.dsc_sub_type == 1) {
 				// TEXT BLOB
 				if (!any_text_blob) {
 					// Save first characterset and collation
@@ -1582,7 +1582,7 @@ void MAKE_desc_from_list(dsc* desc, dsql_nod* node,
 		// If all of the arguments are the same BLOB datattype.
 		desc->dsc_dtype  = max_dtype;
 		desc->dsc_sub_type = max_sub_type;
-		if (max_sub_type = 1) {
+		if (max_sub_type == 1) {
 			// TEXT BLOB
 			desc->dsc_scale = ttype;
 		}
