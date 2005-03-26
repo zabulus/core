28d9b6be7106ff00519edf10314de335e88cb94a
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-03-26 11:41:17 +0000

    Always report system transaction state as tra_commited - missing part. Thanks to Oleg LOA
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@18345 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/tpc.cpp
+++ src/jrd/tpc.cpp
@@ -215,8 +215,9 @@ int TPC_snapshot_state(thread_db* tdbb, SLONG number)
 
 /* if the transaction is older than the oldest
    transaction in our tip cache, it must be committed */
+// hvlad: system transaction always committed too
 
-	if (number < tip_cache->tpc_base) {
+	if (number < tip_cache->tpc_base || number == 0) {
 		return tra_committed;
 	}
 
