0a73f0829f99a941dd03cc82849252b0fdb871ee
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-05-16 09:08:54 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@18918 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.10947 2005-05-16 09:04:13 robocop Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.10948 2005-05-16 09:08:53 robocop Exp $"
 
 BuildType=T
 MajorVer=2
 MinorVer=0
 RevNo=0
-BuildNum=10935
+BuildNum=10936
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
 # Normal builds
@@ -93,7 +93,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=10935
+BuildNum=10936
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
