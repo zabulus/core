328c408faf12de1c7b712cf59570c89b8a1a85c7
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-06-22 22:47:54 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@19561 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/make_icu.bat
+++ builds/win32/make_icu.bat
@@ -4,7 +4,7 @@ set CLEAN=/build
 if "%1"=="CLEAN" (set CLEAN=/rebuild)
 
 if "%VS_VER%"=="msvc6" (
-	@msdev %ROOT_PATH%\extern\icu\source\allinone\allinone.dsw /MAKE "all - Win32 Release" %CLEAN% /OUT icu.log
+	@msdev %ROOT_PATH%\extern\icu\source\allinone\allinone.dsw /MAKE "all - Win32 Release" %CLEAN% /OUT make_icu.log
 ) else (
-	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone.sln %CLEAN% release /OUT icu.log
+	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone.sln %CLEAN% release /OUT make_icu.log
 )
