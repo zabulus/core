56f4e5258c29b6ce101b6a9a6b377379b0e5fd05
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2005-07-19 13:17:01 +0000

    When we say clean lets make sure to include the icu tree as well.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@19699 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/clean_all.bat
+++ builds/win32/clean_all.bat
@@ -8,6 +8,18 @@
 @rmdir /S /Q %ROOT_PATH%\gen 2>nul
 @rmdir /S /Q %ROOT_PATH%\temp 2>nul
 
+@echo Cleaning INTL
+@rmdir /S /Q %ROOT_PATH%\extern\icu\bin
+@rmdir /S /Q %ROOT_PATH%\extern\icu\include
+@rmdir /S /Q %ROOT_PATH%\extern\icu\lib
+@rmdir /S /Q %ROOT_PATH%\extern\icu\source\data\out
+@del /Q %ROOT_PATH%\extern\icu\source\extra\uconv\resources\*.res
+@del %ROOT_PATH%\extern\icu\source\extra\uconv\pkgdatain.txt
+@del %ROOT_PATH%\extern\icu\source\stubdata\stubdatabuilt.txt
+@rmdir /S /Q %ROOT_PATH%\extern\icu\source\test\testdata\out
+@rmdir /S /Q %ROOT_PATH%\extern\icu\source\tools\gencmn\decmn_Win32_Release
+@rmdir /S /Q %ROOT_PATH%\extern\icu\source\tools\genrb\derb_Win32_Release
+
 @echo Cleaning and install build files
 @del *.log 2>nul
 @del %ROOT_PATH%\src\include\gen\autoconfig.h 2>nul
