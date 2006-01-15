4453639412ac1d101c8156dd9442b2e08b9eb961
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-01-15 12:48:30 +0000

    fixed gcc warning
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@21339 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/req.h
+++ src/jrd/req.h
@@ -175,7 +175,7 @@ class jrd_req : public pool_alloc_rpt<record_param, type_req>
 public:
 	jrd_req(JrdMemoryPool* pool) :
 		req_blobs(pool), req_external(*pool), req_access(*pool), req_resources(*pool),
-		req_fors(*pool), req_invariants(*pool), req_trg_name(*pool),
+		req_trg_name(*pool), req_fors(*pool), req_invariants(*pool),
 		req_timestamp(true) {}
 
 	Attachment*	req_attachment;		// database attachment
