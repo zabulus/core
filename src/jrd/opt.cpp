f4fb9e77aefe6b08fe1287cba568509d2f5a7903
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-02-02 12:56:57 +0000

    Fixed a bug with NOT ANY/ALL reported by Alex.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@21527 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/opt.cpp
+++ src/jrd/opt.cpp
@@ -4612,7 +4612,8 @@ static RecordSource* gen_residual_boolean(thread_db* tdbb, OptimizerBlk* opt,
 	for (OptimizerBlk::opt_conjunct* tail = opt->opt_conjuncts.begin(); tail < opt_end; tail++)
 	{
 		jrd_nod* node = tail->opt_conjunct_node;
-		if (!(tail->opt_conjunct_flags & opt_conjunct_used)) {
+		if (!(tail->opt_conjunct_flags & opt_conjunct_used))
+		{
 			compose(&boolean, node, nod_and);
 			tail->opt_conjunct_flags |= opt_conjunct_used;
 		}
@@ -4971,8 +4972,9 @@ static RecordSource* gen_retrieval(thread_db*     tdbb,
 		opt_end = opt->opt_conjuncts.begin() + opt->opt_base_conjuncts;
 		for (tail = opt->opt_conjuncts.begin(); tail < opt_end; tail++) {
 			jrd_nod* node = tail->opt_conjunct_node;
-			if (!(tail->opt_conjunct_flags & opt_conjunct_used)
-				&& OPT_computable(csb, node, -1, false, false))
+			if (!(tail->opt_conjunct_flags & opt_conjunct_used) &&
+				!(node->nod_flags & nod_deoptimize) &&
+				OPT_computable(csb, node, -1, false, false))
 			{
 				compose(return_boolean, node, nod_and);
 				tail->opt_conjunct_flags |= opt_conjunct_used;
@@ -5011,11 +5013,16 @@ static RecordSource* gen_retrieval(thread_db*     tdbb,
 			if ((inversion && expression_contains_stream(csb, node, stream, NULL)) ||
 				(!inversion && OPT_computable(csb, node, stream, false, true)))
 			{
-				compose(&opt_boolean, node, nod_and);
-				tail->opt_conjunct_flags |= opt_conjunct_used;
+				// ANY/ALL boolean should always be left as a residual one
+				if (!(node->nod_flags & nod_deoptimize)) {
+					compose(&opt_boolean, node, nod_and);
+					tail->opt_conjunct_flags |= opt_conjunct_used;
 
-				if (!outer_flag && !(tail->opt_conjunct_flags & opt_conjunct_matched)) {
-					csb_tail->csb_flags |= csb_unmatched;
+					if (!outer_flag &&
+						!(tail->opt_conjunct_flags & opt_conjunct_matched))
+					{
+						csb_tail->csb_flags |= csb_unmatched;
+					}
 				}
 			}
 		}
@@ -5620,8 +5627,9 @@ static bool gen_sort_merge(thread_db* tdbb, OptimizerBlk* opt, RiverStack& org_r
 		for (tail = opt->opt_conjuncts.begin(); tail < end; tail++)
 		{
 			jrd_nod* node1 = tail->opt_conjunct_node;
-			if (!(tail->opt_conjunct_flags & opt_conjunct_used)
-				&& OPT_computable(opt->opt_csb, node1, -1, false, false))
+			if (!(tail->opt_conjunct_flags & opt_conjunct_used) &&
+				!(node1->nod_flags & nod_deoptimize) &&
+				OPT_computable(opt->opt_csb, node1, -1, false, false))
 			{
 				compose(&node, node1, nod_and);
 				tail->opt_conjunct_flags |= opt_conjunct_used;
