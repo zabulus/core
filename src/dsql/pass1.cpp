29a3e2b399dd71803d065fece0a64371f33204a1
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-02-15 13:35:53 +0000

    There's no need to transform NOT ANY to ALL, as NOT ANY effectively uses indices.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@21667 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/dsql/pass1.cpp
+++ src/dsql/pass1.cpp
@@ -5627,6 +5627,7 @@ static dsql_nod* pass1_not(dsql_req* request,
 		case nod_geq_all:
 			node_type = nod_lss_any;
 			break;
+/*
 		case nod_eql_any:
 			if (sub->nod_arg[1]->nod_type == nod_list) {
 				// this is NOT IN (<list>), don't change it
@@ -5651,6 +5652,7 @@ static dsql_nod* pass1_not(dsql_req* request,
 		case nod_geq_any:
 			node_type = nod_lss_all;
 			break;
+*/
 		case nod_between:
 			node_type = nod_or;
 			is_between = true;
