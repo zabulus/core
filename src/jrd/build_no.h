4f43c019c65a1984f013228a68e0c398f5875630
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-04-11 07:18:52 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@22179 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:12589 
+  FORMAL BUILD NUMBER:12590 
 */
 
-#define PRODUCT_VER_STRING "2.0.0.12589"
-#define FILE_VER_STRING "WI-T2.0.0.12589"
-#define LICENSE_VER_STRING "WI-T2.0.0.12589"
-#define FILE_VER_NUMBER 2, 0, 0, 12589
+#define PRODUCT_VER_STRING "2.0.0.12590"
+#define FILE_VER_STRING "WI-T2.0.0.12590"
+#define LICENSE_VER_STRING "WI-T2.0.0.12590"
+#define FILE_VER_NUMBER 2, 0, 0, 12590
 #define FB_MAJOR_VER "2"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "12589"
+#define FB_BUILD_NO "12590"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 2.0 Release Candidate 1"
