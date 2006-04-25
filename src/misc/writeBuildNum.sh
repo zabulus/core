4414d3939573376001ba40fce53180e6b3d3a1b4
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-04-25 18:04:28 +0000

    Update version string.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@22518 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,24 +3,24 @@
 # This file is used both to rebuild the header file and to set the 
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.12936 2006-04-25 17:49:22 alexpeshkoff Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.12937 2006-04-25 18:04:28 dimitr Exp $"
 
 BuildType=T
 MajorVer=2
-MinorVer=0
+MinorVer=1
 RevNo=0
 BuildNum=12782
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
 # Normal builds
-SuffixKind="Release Candidate"
-SuffixVer=1
-BuildSuffix="Firebird 2.0 $SuffixKind $SuffixVer"
+SuffixKind="Unstable"
+SuffixVer=""
+BuildSuffix="Firebird 2.1 $SuffixKind $SuffixVer"
 FIREBIRD_PACKAGE_VERSION=$SuffixKind$SuffixVer
 PRODUCT_VER_STRING="$MajorVer.$MinorVer.$RevNo.$BuildNum"
 else
 # Special builds (daily snapshots, etc)
-BuildSuffix="Firebird 2.0 $SPECIAL_BUILD_SUFFIX"
+BuildSuffix="Firebird 2.1 $SPECIAL_BUILD_SUFFIX"
 FIREBIRD_PACKAGE_VERSION=$SPECIAL_BUILD_SUFFIX
 PRODUCT_VER_STRING="$MajorVer.$MinorVer.$RevNo.$BuildNum-$SPECIAL_BUILD_SUFFIX"
 fi
