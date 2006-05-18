f5d355aebf9e7c21696c83a51e9f68667c0a69e9
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-05-18 08:40:32 +0000

    Just an extra protection from double pool reallocation.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@22847 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/cmp.cpp
+++ src/jrd/cmp.cpp
@@ -2250,6 +2250,7 @@ void CMP_decrement_prc_use_count(thread_db* tdbb, jrd_prc* procedure)
 	{
 		if (procedure->prc_request) {
 			CMP_release(tdbb, procedure->prc_request);
+			procedure->prc_request = NULL;
 		}
 		procedure->prc_flags &= ~PRC_being_altered;
 		MET_remove_procedure(tdbb, procedure->prc_id, procedure);
