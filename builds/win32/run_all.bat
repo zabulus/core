1f0c4561c77ee582b9763ad18ef8960e5a27e019
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-05-28 00:54:07 +0000

    Fix run_all.bat
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@23129 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -1,27 +1,11 @@
 
 @echo off
 
-@if "%1" NEQ "" (set FIREBIRD=%1)
-:: BRS 
-:: Get all the file name when there are spaces
-:: this can be also achieved with %* but I don't know which versions of 
-:: windows allows it
-:LOOP
-	@shift
-	@if "%1"==""  (goto DONE)
-        @set FIREBIRD=%FIREBIRD% %1
-	@goto loop
-:DONE
-
-@echo      FIREBIRD=%FIREBIRD%
-@if "%FIREBIRD%"=="" (goto :HELP & goto :EOF)
-
 set ERRLEV=0
 
 call clean_all
-call prepare
-:: call parse
-call make_boot 
+call make_icu
+call make_boot
 if "%ERRLEV%"=="1" goto :END2
 call make_all 
 if "%ERRLEV%"=="1" goto :END2
