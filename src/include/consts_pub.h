470e459bd3f57b5cbb63085f4cf70f3966f9dc92
aafemt <aafemt@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-08-18 03:46:33 +0000

    License added as Adriano requested
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@24366 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/include/consts_pub.h
+++ src/include/consts_pub.h
@@ -1,3 +1,28 @@
+/*
+ *	MODULE:		consts_pub.h
+ *	DESCRIPTION:	Public constants' definitions
+ *
+ * The contents of this file are subject to the Interbase Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy
+ * of the License at http://www.Inprise.com/IPL.html
+ *
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code was created by Inprise Corporation
+ * and its predecessors. Portions created by Inprise Corporation are
+ * Copyright (C) Inprise Corporation.
+ *
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ *
+ * 18.08.2006 Dimitry Sibiryakov: Extracted it from ibase.h
+ *
+ */
+
 #ifndef INCLUDE_CONSTS_PUB_H
 #define INCLUDE_CONSTS_PUB_H
 
