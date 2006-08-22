fc2bcae4884b15cd8eeebf2dbb0819f5204847c6
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-08-22 11:11:04 +0000

    Backported fix for CORE-900
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@24398 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/jrd.cpp
+++ src/jrd/jrd.cpp
@@ -3420,17 +3420,21 @@ ISC_STATUS GDS_SERVICE_ATTACH(ISC_STATUS* user_status,
 	thread_db* tdbb = JRD_MAIN_set_thread_data(thd_context);
 
 	tdbb->tdbb_status_vector = user_status;
+	tdbb->tdbb_database = NULL;
+	
+	JRD_SS_MUTEX_LOCK;
+	
 	try
 	{
-		tdbb->tdbb_database = NULL;
-	
 		*svc_handle = SVC_attach(service_length, service_name, spb_length, spb);
 	}
 	catch (const std::exception& ex)
 	{
+		JRD_SS_MUTEX_UNLOCK;
 		return error(user_status, ex);
 	}
 
+	JRD_SS_MUTEX_UNLOCK;
 	return return_success(tdbb);
 }
 
