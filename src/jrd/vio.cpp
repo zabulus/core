1e141f38d5814acc18d8b6b9a60f119084561a78
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-08-30 16:59:46 +0000

    Backport fix for CORE-902
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@24470 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/vio.cpp
+++ src/jrd/vio.cpp
@@ -1005,8 +1005,17 @@ void VIO_data(thread_db* tdbb, record_param* rpb, JrdMemoryPool* pool)
 		tail = differences;
 		tail_end = differences + sizeof(differences);
 		if (prior != record)
+			if (record->rec_length < prior->rec_length) {
+				if (record->rec_flags & REC_gc_active) {
+					record = replace_gc_record(rpb->rpb_relation, 
+								&rpb->rpb_record, prior->rec_length);
+				}
+				else {
+					record = realloc_record(rpb->rpb_record, prior->rec_length);
+				}
+			}
 			MOVE_FASTER(prior->rec_data, record->rec_data,
-						format->fmt_length);
+						prior->rec_format->fmt_length);
 	}
 	else
 	{
@@ -3293,8 +3302,17 @@ static void delete_record(thread_db* tdbb, record_param* rpb, SLONG prior_page,
 			tail = differences;
 			tail_end = differences + sizeof(differences);
 			if (prior != record) {
+				if (record->rec_length < prior->rec_length) {
+					if (record->rec_flags & REC_gc_active) {
+						record = replace_gc_record(rpb->rpb_relation, 
+									&rpb->rpb_record, prior->rec_length);
+					}
+					else {
+						record = realloc_record(rpb->rpb_record, prior->rec_length);
+					}
+				}
 				MOVE_FASTER(prior->rec_data, record->rec_data,
-							record->rec_format->fmt_length);
+							prior->rec_format->fmt_length);
 			}
 		}
 		else {
