0802b385f5a18f515b31d19453a6f9fbb073ed01
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-09-12 05:55:56 +0000

    Added makefiles for MSVC8.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@24660 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/make_icu.bat
+++ builds/win32/make_icu.bat
@@ -2,12 +2,17 @@
 
 
 ::Set up the compiler environment
+if DEFINED VS80COMNTOOLS (
+@devenv /? >nul 2>nul
+@if errorlevel 9009 (call "%VS80COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      %VS80COMNTOOLS%vsvars32.bat & echo    has already been executed.)
+) else (
 if DEFINED VS71COMNTOOLS (
 @devenv /? >nul 2>nul
 @if errorlevel 9009 (call "%VS71COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      %VS71COMNTOOLS%vsvars32.bat & echo    has already been executed.)
 ) else (
 @msdev /? >nul 2>nul
 @if errorlevel 9009 (call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat") else (echo MSVC6 vcvars.bat has already been executed.)
+)
 )
 @echo.
 
@@ -40,7 +45,11 @@ if DEFINED VS71COMNTOOLS (
 if "%VS_VER%"=="msvc6" (
 	@msdev %ROOT_PATH%\extern\icu\source\allinone\allinone.dsw /MAKE "all - Win32 Release" %CLEAN% /OUT make_icu.log
 ) else (
+if "%VS_VER%"=="msvc7" (
 	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone.sln %CLEAN% release /OUT make_icu.log
+) else (
+	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone_8.sln %CLEAN% release /OUT make_icu.log
+)
 )
 if errorlevel 1 call :ERROR Release build failed
 @goto :EOF
@@ -52,7 +61,11 @@ if errorlevel 1 call :ERROR Release build failed
 if "%VS_VER%"=="msvc6" (
 	@msdev %ROOT_PATH%\extern\icu\source\allinone\allinone.dsw /MAKE "all - Win32 Debug" %CLEAN% /OUT make_icu.log
 ) else (
+if "%VS_VER%"=="msvc7" (
 	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone.sln %CLEAN% debug /OUT make_icu.log
+) else (
+	@devenv %ROOT_PATH%\extern\icu\source\allinone\allinone_8.sln %CLEAN% debug /OUT make_icu.log
+)
 )
 if errorlevel 1 call :ERROR Debug build failed
 @goto :EOF
