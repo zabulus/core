07a11b5c994d0b93994197f9eb2c37451809eaf4
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-11-09 09:25:33 +0000

    Add missing quotes
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@25375 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/make_icu.bat
+++ builds/win32/make_icu.bat
@@ -4,11 +4,11 @@
 ::Set up the compiler environment
 if DEFINED VS80COMNTOOLS (
 @devenv /? >nul 2>nul
-@if errorlevel 9009 (call "%VS80COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      %VS80COMNTOOLS%vsvars32.bat & echo    has already been executed.)
+@if errorlevel 9009 (call "%VS80COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      "%VS80COMNTOOLS%vsvars32.bat" & echo    has already been executed.)
 ) else (
 if DEFINED VS71COMNTOOLS (
 @devenv /? >nul 2>nul
-@if errorlevel 9009 (call "%VS71COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      %VS71COMNTOOLS%vsvars32.bat & echo    has already been executed.)
+@if errorlevel 9009 (call "%VS71COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      "%VS71COMNTOOLS%vsvars32.bat" & echo    has already been executed.)
 ) else (
 @msdev /? >nul 2>nul
 @if errorlevel 9009 (call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat") else (echo MSVC6 vcvars.bat has already been executed.)
