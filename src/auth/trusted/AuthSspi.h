b04e6039cd655467260d54f61a613c65ac15b8d1
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2006-12-17 15:51:27 +0000

    Added some comments
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@25915 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/auth/trusted/AuthSspi.h
+++ src/auth/trusted/AuthSspi.h
@@ -31,13 +31,20 @@ public:
 	AuthSspi();
 	~AuthSspi();
 
+	// true when has non-empty security context, 
+	// ready to be sent to the other side
 	bool isActive() const
 	{
 		return hasContext;
 	}
 
+	// prepare security context to be sent to the server (used by client)
 	bool request(DataHolder& data);
+	
+	// accept security context from the client (used by server)
 	bool accept(DataHolder& data);
+
+	// returns windows user name, matching accepted security context
 	bool getLogin(Firebird::string& login);
 };
 
