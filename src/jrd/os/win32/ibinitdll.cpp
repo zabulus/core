912fac1ab14594e6805e5d3550ecd91c2412677d
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-01-06 09:11:52 +0000

    1) Fixed CORE-1079. 2) Cleanup.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@26110 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/os/win32/ibinitdll.cpp
+++ src/jrd/os/win32/ibinitdll.cpp
@@ -25,41 +25,23 @@
 #include <windows.h>
 #include "../jrd/common.h"
 #include "../../utilities/install/registry.h"
+#include "../jrd/jrd_proto.h"
 #include "../jrd/thread_proto.h"
-
-HINSTANCE hIBDLLInstance;
-
-#ifdef SUPERSERVER
-
-#ifdef  _MSC_VER
-BOOL WINAPI _CRT_INIT(HINSTANCE, DWORD, LPVOID);
-#else
-BOOL WINAPI _CRT_INIT(HINSTANCE HIdummy, DWORD DWdummy, LPVOID LPVdummy)
-{
-	return TRUE;
-}
-#endif
-
-#else /* SUPERSERVER */
 
 BOOL WINAPI DllMain(HINSTANCE h, DWORD reason, LPVOID reserved)
 {
-	/* save instance value */
-	hIBDLLInstance = h;
-
 	switch (reason)	{
-
-	case DLL_PROCESS_ATTACH:
-		break;
 
 	case DLL_PROCESS_DETACH:
 #ifdef EMBEDDED
 		JRD_shutdown_all(false);
 #endif
+		MemoryPool::deletePool(getDefaultMemoryPool());
+		break;
+
+	default:
 		break;
 	}
 
 	return TRUE;
 }
-
-#endif /* SUPERSERVER */
