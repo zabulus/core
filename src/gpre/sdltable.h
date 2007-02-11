15b2e64e905c23bd47798e2c3f462e74999e5e78
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-02-11 09:23:32 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@26543 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/gpre/sdltable.h
+++ src/gpre/sdltable.h
@@ -16,40 +16,40 @@
  * All Rights Reserved.
  * Contributor(s): ______________________________________.
  */
-"gds__sdl_eoc",					/* -1 */
-	"gds__sdl_version1",		/* 1 */
-	"gds__sdl_relation",		/* 2 */
-	"gds__sdl_rid",				/* 3 */
-	"gds__sdl_field",			/* 4 */
-	"gds__sdl_fid",				/* 5 */
-	"gds__sdl_struct",			/* 6 */
-	"gds__sdl_variable",		/* 7 */
-	"gds__sdl_scalar",			/* 8 */
-	"gds__sdl_tiny_integer",	/* 9 */
-	"gds__sdl_short_integer",	/* 10 */
-	"gds__sdl_long_integer",	/* 11 */
-	"gds__sdl_literal",			/* 12 */
-	"gds__sdl_add",				/* 13 */
-	"gds__sdl_subtract",		/* 14 */
-	"gds__sdl_multiply",		/* 15 */
-	"gds__sdl_divide",			/* 16 */
-	"gds__sdl_negate",			/* 17 */
-	"gds__sdl_eql",				/* 18 */
-	"gds__sdl_neq",				/* 19 */
-	"gds__sdl_gtr",				/* 20 */
-	"gds__sdl_geq",				/* 21 */
-	"gds__sdl_lss",				/* 22 */
-	"gds__sdl_leq",				/* 23 */
-	"gds__sdl_and",				/* 24 */
-	"gds__sdl_or",				/* 25 */
-	"gds__sdl_not",				/* 26 */
-	"gds__sdl_while",			/* 27 */
-	"gds__sdl_assignment",		/* 28 */
-	"gds__sdl_label",			/* 29 */
-	"gds__sdl_leave",			/* 30 */
-	"gds__sdl_begin",			/* 31 */
-	"gds__sdl_end",				/* 32 */
-	"gds__sdl_do3",				/* 33 */
-	"gds__sdl_do2",				/* 34 */
-	"gds__sdl_do1",				/* 35 */
-	"gds__sdl_element",			/* 36 */
+"gds__sdl_eoc",					// -1
+	"gds__sdl_version1",		// 1
+	"gds__sdl_relation",		// 2
+	"gds__sdl_rid",				// 3
+	"gds__sdl_field",			// 4
+	"gds__sdl_fid",				// 5
+	"gds__sdl_struct",			// 6
+	"gds__sdl_variable",		// 7
+	"gds__sdl_scalar",			// 8
+	"gds__sdl_tiny_integer",	// 9
+	"gds__sdl_short_integer",	// 10
+	"gds__sdl_long_integer",	// 11
+	"gds__sdl_literal",			// 12
+	"gds__sdl_add",				// 13
+	"gds__sdl_subtract",		// 14
+	"gds__sdl_multiply",		// 15
+	"gds__sdl_divide",			// 16
+	"gds__sdl_negate",			// 17
+	"gds__sdl_eql",				// 18
+	"gds__sdl_neq",				// 19
+	"gds__sdl_gtr",				// 20
+	"gds__sdl_geq",				// 21
+	"gds__sdl_lss",				// 22
+	"gds__sdl_leq",				// 23
+	"gds__sdl_and",				// 24
+	"gds__sdl_or",				// 25
+	"gds__sdl_not",				// 26
+	"gds__sdl_while",			// 27
+	"gds__sdl_assignment",		// 28
+	"gds__sdl_label",			// 29
+	"gds__sdl_leave",			// 30
+	"gds__sdl_begin",			// 31
+	"gds__sdl_end",				// 32
+	"gds__sdl_do3",				// 33
+	"gds__sdl_do2",				// 34
+	"gds__sdl_do1",				// 35
+	"gds__sdl_element",			// 36
