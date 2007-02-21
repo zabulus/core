1cfb083e919d438176a7c10ae80d0f68a369f963
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-02-21 12:39:41 +0000

    Fixed CORE-1140.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@26658 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/jrd.cpp
+++ src/jrd/jrd.cpp
@@ -4869,6 +4869,7 @@ static ISC_STATUS commit(
 		next = transaction->tra_sibling;
 		check_database(tdbb, transaction->tra_attachment, user_status);
 		tdbb->tdbb_status_vector = ptr;
+		tdbb->tdbb_transaction = transaction;
 		TRA_commit(tdbb, transaction, retaining_flag);
 		Database* dbb = tdbb->tdbb_database;
 		--dbb->dbb_use_count;
@@ -5887,6 +5888,7 @@ static bool rollback(thread_db*	tdbb,
 
 		try {
 		tdbb->tdbb_status_vector = status_vector;
+		tdbb->tdbb_transaction = transaction;
 		TRA_rollback(tdbb, transaction, retaining_flag, false);
 		Database* dbb = tdbb->tdbb_database;
 		--dbb->dbb_use_count;
