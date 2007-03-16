685db5db011a2c69ca2cc6fddc18c7efc790c866
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-03-16 11:49:45 +0000

    Update manual installation batch files to support installation as a non-default service name.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@27017 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/install/arch-specific/win32/install_classic.bat
+++ builds/install/arch-specific/win32/install_classic.bat
@@ -1,3 +1,15 @@
+@echo off
+if "%1"=="" (
 instreg install -z
-instsvc install -auto -classic -guardian -z
-instsvc start
+)
+setlocal
+set INSTALL_SERVICE=install -auto -classic -guardian -z
+set START_SERVICE=start
+if not "%1"=="" (
+set INSTALL_SERVICE=%INSTALL_SERVICE% -n %1
+set START_SERVICE=%START_SERVICE% -n %1
+)
+instsvc %INSTALL_SERVICE%
+instsvc %START_SERVICE%
+endlocal
+
