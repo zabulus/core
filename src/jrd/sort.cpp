06e525e83324492cabafad3c6ad9f7484301c931
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-04-01 14:52:25 +0000

    Misc
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@27302 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/sort.cpp
+++ src/jrd/sort.cpp
@@ -94,9 +94,9 @@ const ULONG MAX_TEMPFILE_SIZE		= 1073741824;	// 1GB
 // the size of sr_bckptr (everything before sort_record) in bytes
 #define SIZEOF_SR_BCKPTR OFFSET(sr*, sr_sort_record)
 // the size of sr_bckptr in # of 32 bit longwords
-#define SIZEOF_SR_BCKPTR_IN_LONGS (SIZEOF_SR_BCKPTR / sizeof(SLONG))
+#define SIZEOF_SR_BCKPTR_IN_LONGS static_cast<signed>(SIZEOF_SR_BCKPTR / sizeof(SLONG))
 // offset in array of pointers to back record pointer (sr_bckptr)
-#define BACK_OFFSET (-static_cast<int>(SIZEOF_SR_BCKPTR / sizeof(SLONG*)))
+#define BACK_OFFSET (-static_cast<signed>(SIZEOF_SR_BCKPTR / sizeof(SLONG*)))
 
 #define DIFF_LONGS(a, b)         ((a) - (b))
 #define SWAP_LONGS(a, b, t)       {t = a; a = b; b = t;}
