05349caa388e7ac54d9f749e982f969a72976f94
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-08-13 03:47:10 +0000

    Removed the unconditional debugging info, moved the tag.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@28962 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/vector.h
+++ src/common/classes/vector.h
@@ -24,7 +24,7 @@
  *  Contributor(s): ______________________________________.
  *
  *
- *  $Id: vector.h,v 1.15.4.1 2007-07-27 13:31:42 alexpeshkoff Exp $
+ *  $Id: vector.h,v 1.15.4.2 2007-08-13 03:47:10 dimitr Exp $
  *
  */
 
@@ -79,9 +79,6 @@ public:
 	}
 	void join(const Vector<T, Capacity>& L) {
 		fb_assert(count + L.count <= Capacity);
-#ifndef BOOT_BUILD
-		gds__log("Join:%p %8d, L:%p %8d", data, count, L.data, L.count);
-#endif
 		memcpy(data + count, L.data, sizeof(T) * L.count);
 		count += L.count;
 	}
