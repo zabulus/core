a1f6ee10ea32f6e762ce2419854d00c7a64ddd98
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-09-10 13:55:54 +0000

    Allow run_all.bat to pass a variable through to each batch file it calls.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@29391 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -4,13 +4,13 @@
 set ERRLEV=0
 
 if not defined FB_NOCLEAN (call clean_all)
-call make_icu
+call make_icu %1
 if "%ERRLEV%"=="1" goto :END
-call make_boot
+call make_boot %1
 if "%ERRLEV%"=="1" goto :END
-call make_all
+call make_all %1
 if "%ERRLEV%"=="1" goto :END
-call make_examples
+call make_examples %1
 if "%ERRLEV%"=="1" goto :END
 pushd ..\install\arch-specific\win32
 call BuildExecutableInstall ISX ZIP EMB
