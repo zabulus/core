a9932a98c7b3dc94015fc4aef802d862d50cc191
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-10-10 14:00:25 +0000

    Make run_all a little more useful.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@29959 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -1,20 +1,39 @@
 
 @echo off
 
+:: Reset or clear some variables, as appropriate.
 set ERRLEV=0
+set FB_NOCLEAN=
+set FBBUILD_BUILDTYPE=release
+set FBBUILD_INCLUDE_PDB=
 
+:: Read the command line
+for %%v in ( %* )  do (
+( if /I "%%v"=="NOCLEAN" (set FB_NOCLEAN=1) )
+( if /I "%%v"=="DEBUG" (set FBBUILD_BUILDTYPE=debug) )
+( if /I "%%v"=="PDB" (set FBBUILD_INCLUDE_PDB=1) )
+)
+
+:: Go to work
 if not defined FB_NOCLEAN (call clean_all)
-:: We do not support debug builds of icu, so we don't pass %1
+:: We do not support debug builds of icu, so we don't pass %FBBUILD_BUILDTYPE%
 call make_icu
 if "%ERRLEV%"=="1" goto :END
-call make_boot %1
+call make_boot %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
-call make_all %1
+call make_all %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
-call make_examples %1
+call make_examples %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
+
+:: Package everything up
 pushd ..\install\arch-specific\win32
-call BuildExecutableInstall ISX ZIP EMB %1
+call BuildExecutableInstall ISX ZIP EMB %FBBUILD_BUILDTYPE%
+if "%ERRLEV%"=="1" (popd & goto :END)
+if defined FBBUILD_INCLUDE_PDB (
+set FBBUILD_PACKAGE_NUMBER=
+call BuildExecutableInstall ISX ZIP EMB %FBBUILD_BUILDTYPE% PDB
+)
 popd
 
 goto:END
