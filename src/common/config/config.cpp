4897658129b2902a8175aa47c6751ada16b62b03
skidder <skidder@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-10-28 14:35:16 +0000

    Allow local protocol to work with servers running both under restricted accounts (e.g. Vista regular user) and unrestricted accounts (e.g. LocalService)
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@30183 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/config/config.cpp
+++ src/common/config/config.cpp
@@ -51,8 +51,6 @@ const char*	AmNative	= "native";
 const char*	AmTrusted	= "trusted";
 const char*	AmMixed		= "mixed";
 
-char defIpcName[MAXPATHLEN]; 
-
 const ConfigImpl::ConfigEntry ConfigImpl::entries[] =
 {
 	{TYPE_STRING,		"RootDirectory",			(ConfigValue) 0},
@@ -97,7 +95,7 @@ const ConfigImpl::ConfigEntry ConfigImpl::entries[] =
 	{TYPE_STRING,		"RemoteServiceName",		(ConfigValue) FB_SERVICE_NAME},
 	{TYPE_INTEGER,		"RemoteServicePort",		(ConfigValue) 0},
 	{TYPE_STRING,		"RemotePipeName",			(ConfigValue) FB_PIPE_NAME},
-	{TYPE_STRING,		"IpcName",					(ConfigValue) defIpcName},
+	{TYPE_STRING,		"IpcName",					(ConfigValue) FB_IPC_NAME},
 #ifdef WIN_NT
 	{TYPE_INTEGER,		"MaxUnflushedWrites",		(ConfigValue) 100},
 	{TYPE_INTEGER,		"MaxUnflushedWriteTime",	(ConfigValue) 5},
@@ -183,11 +181,6 @@ ConfigImpl::ConfigImpl(MemoryPool& p) : ConfigRoot(p)
 
 	string val_sep = ",";
 	file.setConfigFilePath(getConfigFilePath());
-
-	strncpy(defIpcName, FB_IPC_NAME, sizeof(defIpcName));
-#ifdef WIN_NT
-	fb_utils::prefix_kernel_object_name(defIpcName, sizeof(defIpcName));
-#endif
 
 	/* Iterate through the known configuration entries */
 
