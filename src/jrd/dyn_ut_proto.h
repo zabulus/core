326e34152f830f2cbccf7921777ee48c84e3792c
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-11-15 18:30:35 +0000

    Sync'ed prototype with the definition.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@30537 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/dyn_ut_proto.h
+++ src/jrd/dyn_ut_proto.h
@@ -37,7 +37,7 @@ void	DYN_UTIL_generate_field_name(Jrd::thread_db*, Jrd::Global*, TEXT*);
 void	DYN_UTIL_generate_field_name(Jrd::thread_db*, Jrd::Global*, Firebird::MetaName&);
 void	DYN_UTIL_generate_constraint_name(Jrd::thread_db*, Jrd::Global*, Firebird::MetaName&);
 SINT64	DYN_UTIL_gen_unique_id(Jrd::thread_db*, Jrd::Global*, SSHORT,
-			const SCHAR*, BLK*);
+							   const SCHAR*, Jrd::jrd_req**);
 bool    DYN_UTIL_is_array(Jrd::thread_db*, Jrd::Global*, const Firebird::MetaName&);
 void	DYN_UTIL_copy_domain(Jrd::thread_db*, Jrd::Global* gbl,
 			const Firebird::MetaName&, const Firebird::MetaName&);
