fdfc3de8c98161c0c3a5dfae9fe4a5f6baf02ba2
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-12-21 12:22:22 +0000

    MInor tweaks to a) speed up testing the installer, and b) document the available options.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@31262 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -6,13 +6,25 @@ set ERRLEV=0
 set FB_NOCLEAN=
 set FBBUILD_BUILDTYPE=release
 set FBBUILD_INCLUDE_PDB=
+set FBBUILD_MAKE_KITS_ONLY=
+
+::Check if on-line help is required
+for %%v in ( %1 %2 %3 %4 %5 %6 %7 %8 %9 )  do (
+  ( @if /I "%%v"=="-h" (goto :HELP & goto :EOF) )
+  ( @if /I "%%v"=="/h" (goto :HELP & goto :EOF) )
+  ( @if /I "%%v"=="HELP" (goto :HELP & goto :EOF) )
+)
+
 
 :: Read the command line
 for %%v in ( %* )  do (
 ( if /I "%%v"=="NOCLEAN" (set FB_NOCLEAN=1) )
 ( if /I "%%v"=="DEBUG" (set FBBUILD_BUILDTYPE=debug) )
 ( if /I "%%v"=="PDB" (set FBBUILD_INCLUDE_PDB=1) )
+( if /I "%%v"=="REPACK" (set FBBUILD_MAKE_KITS_ONLY=1) )
 )
+
+if defined FBBUILD_MAKE_KITS_ONLY (goto :MAKE_KITS & goto :EOF)
 
 :: Go to work
 if not defined FB_NOCLEAN (call clean_all)
@@ -26,6 +38,7 @@ if "%ERRLEV%"=="1" goto :END
 call make_examples %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
 
+:MAKE_KITS
 :: Package everything up
 pushd ..\install\arch-specific\win32
 call BuildExecutableInstall ISX ZIP EMB %FBBUILD_BUILDTYPE%
@@ -36,7 +49,22 @@ call BuildExecutableInstall ISX ZIP EMB %FBBUILD_BUILDTYPE% PDB
 )
 popd
 
-goto:END
+goto :END
 
+:HELP
+@echo.
+@echo The following params may be passed:
+@echo.
+@echo    NOCLEAN - don't run CLEAN_ALL.BAT
+@echo.
+@echo    DEBUG   - Do a DEBUG build (for experienced developers only.)
+@echo              This switch is not needed to debug Firebird.
+@echo.
+@echo    PDB     - Create PDB packages as well as standard kits
+@echo.
+@echo    REPACK  - Don't build - just repack kits.
+@echo.
+@goto :EOF
 
 :END
+
