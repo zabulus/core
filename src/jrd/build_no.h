8ec671177d4489b0d7acc48d2678327dc6ad9f08
sboyd <sboyd@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2007-12-27 21:08:42 +0000

    increment build number
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@31368 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:17717 
+  FORMAL BUILD NUMBER:17718 
 */
 
-#define PRODUCT_VER_STRING "2.1.0.17717"
-#define FILE_VER_STRING "WI-T2.1.0.17717"
-#define LICENSE_VER_STRING "WI-T2.1.0.17717"
-#define FILE_VER_NUMBER 2, 1, 0, 17717
+#define PRODUCT_VER_STRING "2.1.0.17718"
+#define FILE_VER_STRING "WI-T2.1.0.17718"
+#define LICENSE_VER_STRING "WI-T2.1.0.17718"
+#define FILE_VER_NUMBER 2, 1, 0, 17718
 #define FB_MAJOR_VER "2"
 #define FB_MINOR_VER "1"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "17717"
+#define FB_BUILD_NO "17718"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 2.1 Release Candidate 1"
