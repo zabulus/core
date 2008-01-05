7a94cae289e47f3898945da556edc9d1e9641c06
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-01-05 08:19:44 +0000

    No need to use the class name in a member function declaration inside the class.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@31427 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/semaphore.h
+++ src/common/classes/semaphore.h
@@ -115,7 +115,7 @@ private:
 	bool  init;
 public:
 #ifdef MIXED_SEMAPHORE_AND_FILE_HANDLE
-	static bool SignalSafeSemaphore::checkHandle(int n);
+	static bool checkHandle(int n);
 #endif
 	SignalSafeSemaphore()
 		: init(false)
