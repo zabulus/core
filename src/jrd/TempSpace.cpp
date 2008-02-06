06d160aaedc4178d9ba4a5638a133cba0750c154
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-02-06 19:30:30 +0000

    Let's not infinitely write regular messaging to the log file.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@32360 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/TempSpace.cpp
+++ src/jrd/TempSpace.cpp
@@ -394,11 +394,6 @@ TempFile* TempSpace::setupFile(size_t size)
 			if (i < tempDirs->getCount() - 1)
 			{
 				// no room, let's try another directory
-				Firebird::string text;
-				text.printf("Temporary directory %s has no free space, "
-							"attempting to switch to the next configured one",
-							directory.c_str());
-				gds__log(text.c_str());
 				file = NULL;
 			}
 			else
