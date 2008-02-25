8a268fad5e783291d282ac4a56f286bd102353cd
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-02-25 09:29:43 +0000

    Gosh! Isn't it fun maintaining four separate branches.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@32769 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/install/arch-specific/win32/install_classic.bat
+++ builds/install/arch-specific/win32/install_classic.bat
@@ -3,7 +3,7 @@ if "%1"=="" (
 instreg install -z
 )
 setlocal
-set INSTALL_SERVICE=install -auto -classic -guardian -z
+set INSTALL_SERVICE=install -auto -classic -z
 set START_SERVICE=start
 if not "%1"=="" (
 set INSTALL_SERVICE=%INSTALL_SERVICE% -n %1
