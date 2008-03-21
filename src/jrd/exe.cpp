a5ccb32326c9ffda58fc36e3fe3e4d74df5bc0f4
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-03-21 17:05:21 +0000

    Fixed CORE-1798 - RDB$DB_KEY evaluates to NULL in INSERT ... RETURNING
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@33630 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/exe.cpp
+++ src/jrd/exe.cpp
@@ -3777,6 +3777,8 @@ static jrd_nod* store(thread_db* tdbb, jrd_nod* node, SSHORT which_trig)
 			}
 		}
 
+		rpb->rpb_number.setValid(true);
+
 		if (relation->rel_post_store &&
 			(which_trig != PRE_TRIG) &&
 			(trigger = execute_triggers(tdbb, &relation->rel_post_store,
