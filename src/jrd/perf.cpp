44126e5ac5aafbc4bb666a3436ffa45b91536c77
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-03-28 13:33:39 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@33845 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/perf.cpp
+++ src/jrd/perf.cpp
@@ -40,12 +40,10 @@
 #if defined(TIME_WITH_SYS_TIME)
 #include <sys/time.h>
 #include <time.h>
-#else
-#if defined(HAVE_SYS_TIME_H)
+#elif defined(HAVE_SYS_TIME_H)
 #include <sys/time.h>
 #else
 #include <time.h>
-#endif
 #endif
 
 #ifdef HAVE_SYS_TIMEB_H
