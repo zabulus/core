2011315d4030370ffa64329e969231abf249a572
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-05-07 05:33:48 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@35077 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/burp/burp.h
+++ src/burp/burp.h
@@ -945,7 +945,7 @@ public:
 	isc_req_handle	handles_write_function_args_req_handle1;
 	isc_req_handle	handles_write_function_args_req_handle2;
 	isc_req_handle	handles_write_procedure_prms_req_handle1;
-	USHORT			hdr_forced_writes;
+	bool			hdr_forced_writes;
 	TEXT			database_security_class[GDS_NAME_LEN]; // To save database security class for deferred update 
 	
 	static inline BurpGlobals* getSpecific() {
