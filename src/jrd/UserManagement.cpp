2460f493d06d19f6708f98a2c7771b097a16eb3d
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-05-21 12:19:38 +0000

    added files forgotten in previous commit
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@35481 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

---
+++ src/jrd/UserManagement.cpp
@@ -0,0 +1,99 @@
+/*
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Alex Peshkov
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2008 Alex Peshkov <alexpeshkoff@users.sf.net>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ */
+
+#include "firebird.h"
+#include "../common/classes/ClumpletWriter.h"
+#include "../jrd/UserManagement.h"
+#include "../jrd/common.h"
+#include "../jrd/jrd.h"
+#include "../jrd/jrd_pwd.h"
+#include "../utilities/gsec/gsec.h"
+#include "../utilities/gsec/secur_proto.h"
+
+
+using namespace Jrd;
+
+UserManagement::UserManagement(thread_db* tdbb)
+{
+	char securityDatabaseName[MAXPATHLEN];
+	SecurityDatabase::getPath(securityDatabaseName);
+	ISC_STATUS_ARRAY status;
+
+	Firebird::ClumpletWriter dpb(Firebird::ClumpletReader::Tagged, MAX_DPB_SIZE, isc_dpb_version1);
+	dpb.insertByte(isc_dpb_gsec_attach, TRUE);
+	dpb.insertString(isc_dpb_trusted_auth, tdbb->getAttachment()->att_user->usr_user_name);
+
+	if (isc_attach_database(status, 0, securityDatabaseName, &database, 
+							dpb.getBufferLength(), reinterpret_cast<const char*>(dpb.getBuffer())))
+	{
+		Firebird::status_exception::raise(status);
+	}
+
+	if (isc_start_transaction(status, &transaction, 1, &database, 0, NULL))
+	{
+		Firebird::status_exception::raise(status);
+	}
+}
+
+void UserManagement::commit()
+{
+	ISC_STATUS_ARRAY status;
+	if (transaction) 
+	{
+		// Commit transaction in security database
+		if (isc_commit_transaction(status, &transaction)) 
+		{
+			Firebird::status_exception::raise(status);
+		}
+		transaction = 0;
+	}
+}
+
+UserManagement::~UserManagement()
+{
+	ISC_STATUS_ARRAY status;
+	if (transaction) 
+	{
+		// Rollback transaction in security database ...
+		if (isc_rollback_transaction(status, &transaction)) 
+		{
+			Firebird::status_exception::raise(status);
+		}
+	}
+
+	if (database) 
+	{
+		if (isc_detach_database(status, &database)) 
+		{
+			Firebird::status_exception::raise(status);
+		}
+	}
+}
+
+int UserManagement::execute(ISC_STATUS* status, internal_user_data* u)
+{
+#if (defined BOOT_BUILD || defined EMBEDDED)
+	Firebird::status_exception::raise(isc_wish_list, isc_arg_end);
+#else
+	return (!u->user_name_entered) ? GsecMsg18 : SECURITY_exec_line(status, database, transaction, u, NULL, NULL);
+#endif
+}
---
+++ src/jrd/UserManagement.h
@@ -0,0 +1,50 @@
+/*
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Alex Peshkov
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2008 Alex Peshkov <alexpeshkoff@users.sf.net>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________.
+ */
+
+#ifndef JRD_USER_MANAGEMENT_H
+#define JRD_USER_MANAGEMENT_H
+
+#include "firebird.h"
+#include "../jrd/ibase.h"
+
+struct internal_user_data;
+
+namespace Jrd {
+
+class thread_db;
+
+// User management argument for deferred work
+class UserManagement
+{
+public:
+	UserManagement(thread_db* tdbb);
+	~UserManagement();
+private:
+	FB_API_HANDLE database, transaction;
+public:
+	int execute(ISC_STATUS* status, internal_user_data* u);
+	void commit();
+};
+
+};
+
+#endif // JRD_USER_MANAGEMENT_H
