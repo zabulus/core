e29065533258f2332f45f829ba6a88124ad4b8b1
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-06-18 17:09:50 +0000

    SQLSTATE correction, as suggested by Bill.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@36174 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/msgs/system_errors2.sql
+++ src/msgs/system_errors2.sql
@@ -598,7 +598,7 @@ set bulk_insert INSERT INTO SYSTEM_ERRORS (SQL_CODE, SQL_CLASS, SQL_SUBCLASS, FA
 (-802, '22', '001', 0, 595, 'blob_truncation', NULL, NULL)
 (-802, '22', '003', 0, 596, 'numeric_out_of_range', NULL, NULL)
 (-901, 'HY', 'T00', 0, 597, 'shutdown_timeout', NULL, NULL)
-(-901, '42', '000', 0, 598, 'att_handle_busy', NULL, NULL)
+(-901, 'HY', '010', 0, 598, 'att_handle_busy', NULL, NULL)
 (-901, '39', '000', 0, 599, 'bad_udf_freeit', NULL, NULL)
 -- GFIX
 (-901, '00', '000', 3, 1, 'gfix_db_name', NULL, NULL)
