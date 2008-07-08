41151a122597fa993a185b86addd7844a3d34a9a
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-07-08 09:02:30 +0000

    Backported fix for CORE-1983: Out of memory condition in OS causes AV
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_1_Release@36498 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/alloc.cpp
+++ src/common/classes/alloc.cpp
@@ -442,30 +442,37 @@ void* MemoryPool::external_alloc(size_t &size)
 	return VirtualAlloc(NULL, size, MEM_COMMIT, 
 						PAGE_READWRITE);
 # elif defined (HAVE_MMAP) && !defined(SOLARIS)
-	size = FB_ALIGN(size, get_map_page_size());
-#  ifdef MAP_ANONYMOUS
-	return  mmap(NULL, size, PROT_READ | PROT_WRITE, 
-				MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-#  else	
-	// This code is needed for Solaris 2.6, AFAIK  (only?)
-	if (dev_zero_fd < 0)
-		dev_zero_fd = open("/dev/zero", O_RDWR);
-	return mmap(NULL, size, PROT_READ | PROT_WRITE, 
-				MAP_PRIVATE, dev_zero_fd, 0);
-#  endif //MAP_ANONYMOUS
-# elif  defined(SOLARIS)
 
 // No successful return from mmap()  will  return    the value MAP_FAILED.
-//The  symbol  MAP_FAILED  is  defined  in  the header     <sys/mman.h>
-//Solaris 2.9 #define MAP_FAILED      ((void *) -1)
-
+//The  symbol  MAP_FAILED  is  defined:
+//#define MAP_FAILED      ((void *) -1)
 
 	size = FB_ALIGN(size, get_map_page_size());
 	void *result = NULL;
 #  ifdef MAP_ANONYMOUS
-    
+	result = mmap(NULL, size, PROT_READ | PROT_WRITE, 
+				  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#  else	
+	// This code is needed for Solaris 2.6, AFAIK  (only?)
+	if (dev_zero_fd < 0)
+		dev_zero_fd = open("/dev/zero", O_RDWR);
+	result = mmap(NULL, size, PROT_READ | PROT_WRITE, 
+				  MAP_PRIVATE, dev_zero_fd, 0);
+#  endif //MAP_ANONYMOUS
+	return result == MAP_FAILED ? NULL : result;	
+# elif  defined(SOLARIS)
+	size = FB_ALIGN(size, get_map_page_size());
+	void *result = NULL;
+#  ifdef MAP_ANONYMOUS
 	result = mmap(0, size, PROT_READ | PROT_WRITE, 
 					MAP_PRIVATE | MAP_ANON , -1, 0);
+#  else	
+	// This code is needed for Solaris 2.6, AFAIK
+	if (dev_zero_fd < 0)
+		dev_zero_fd = open("/dev/zero", O_RDWR);
+	result = mmap(NULL, size, PROT_READ | PROT_WRITE, 
+					MAP_PRIVATE, dev_zero_fd, 0);
+#  endif //MAP_ANONYMOUS
 	if (result == MAP_FAILED) {
 		// failure happens!
 		return NULL;
@@ -473,19 +480,6 @@ void* MemoryPool::external_alloc(size_t &size)
 	else {
 		return result;	
 	}	
-#  else	
-	// This code is needed for Solaris 2.6, AFAIK
-	if (dev_zero_fd < 0)
-		dev_zero_fd = open("/dev/zero", O_RDWR);
-	result = mmap(NULL, size, PROT_READ | PROT_WRITE, 
-					MAP_PRIVATE, dev_zero_fd, 0);
-	if (result == MAP_FAILED) {
-		return NULL;
-	}	
-	else {
-		return result;	
-	}			
-#  endif //MAP_ANONYMOUS
 # else
 	return malloc(size);
 # endif
