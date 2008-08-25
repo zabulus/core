9de8d2377aec8fc541f2d8bffb915d654ad071bf
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-08-25 02:29:37 +0000

    Rollback backport of nod_assignment changes - things are not so simple
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_1_Release@36894 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/par.cpp
+++ src/jrd/par.cpp
@@ -2629,21 +2629,15 @@ static jrd_nod* parse(thread_db* tdbb, CompilerScratch* csb, USHORT expected,
 
 	const SSHORT sub_type = sub_type_table[blr_operator];
 
- 	// ASF: blr_assignment is recognized as STATEMENT and VALUE, but is marked as STATEMENT
- 	// in blrtable.cpp.
- 	if (expected && (expected != type_table[blr_operator]) &&
- 		!(blr_operator == nod_assignment && expected == VALUE))
- 	{
- 		if (expected_optional)
- 		{
- 			if (expected_optional != type_table[blr_operator] &&
- 				!(blr_operator == nod_assignment && expected_optional == VALUE))
- 			{
+ 	if (expected && (expected != type_table[blr_operator])) {
+ 		if (expected_optional) {
+ 			if (expected_optional != type_table[blr_operator]) {
 				syntax_error(csb, elements[expected]);
 			}
 		}
-		else
+ 		else {
 			syntax_error(csb, elements[expected]);
+		}
 	}
 
 /* If there is a length given in the length table, pre-allocate
