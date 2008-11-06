7117670e6894ba2707fecc7f64530e3110dd7de8
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-11-06 17:09:02 +0000

    Fixed CORE-2171: Column MON$CALLER_ID of table MON$CALL_STACK reports invalid ID.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@37846 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/DatabaseSnapshot.cpp
+++ src/jrd/DatabaseSnapshot.cpp
@@ -801,6 +801,7 @@ const char* DatabaseSnapshot::checkNull(int rid, int fid, const char* source, si
 		switch (fid)
 		{
 		case f_mon_call_caller_id:
+			return (*(SINT64*) source) ? source : NULL;
 		case f_mon_call_type:
 		case f_mon_call_src_line:
 		case f_mon_call_src_column:
@@ -1187,10 +1188,10 @@ void DatabaseSnapshot::putCall(const jrd_req* request,
 	writer.insertBigInt(f_mon_call_stmt_id, getGlobalId(statement->req_id));
 	// caller id
 	if (statement == request->req_caller) {
-		writer.insertInt(f_mon_call_caller_id, 0);
+		writer.insertBigInt(f_mon_call_caller_id, 0);
 	}
 	else {
-		writer.insertInt(f_mon_call_caller_id, request->req_caller->req_id);
+		writer.insertBigInt(f_mon_call_caller_id, getGlobalId(request->req_caller->req_id));
 	}
 	// object name/type
 	if (request->req_procedure) {
