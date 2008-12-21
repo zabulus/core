1c3a379736ae1f53b09702a8b4f5ce1e4893d648
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2008-12-21 06:13:12 +0000

    There's no need to check whether a pointer is null before applying delete or delete[].
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@38811 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/utilities/install/install_svc.cpp
+++ src/utilities/install/install_svc.cpp
@@ -700,8 +700,8 @@ static void svc_query(const char* name, const char* display_name, SC_HANDLE mana
 		}
 		else
 			svc_error(GetLastError(), "QueryServiceConfig", NULL);
-		if (qsc)
-			delete[] (UCHAR*) qsc;
+
+		delete[] (UCHAR*) qsc;
 
 		CloseServiceHandle(service);
 	}
