2d175fc5583800112b29479751a993d472b47ccb
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-01-15 02:10:43 +0000

    This is C-compatible header
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@39445 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/intlobj_new.h
+++ src/jrd/intlobj_new.h
@@ -211,10 +211,14 @@ typedef void (*pfn_INTL_cv_destroy) (
 /* csconvert version */
 #define CSCONVERT_VERSION_1	1
 
+#ifdef __cplusplus
 namespace
 {
+#endif
 	struct CsConvertImpl;	// forward declaration - refers to diff struct in diff modules!
+#ifdef __cplusplus
 }
+#endif
 
 struct csconvert
 {
