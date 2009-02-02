e499c9b806446cb996fed144fb3826136f485e4d
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-02-02 12:06:51 +0000

    Make GCC's life easier
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@39863 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/DatabaseSnapshot.cpp
+++ src/jrd/DatabaseSnapshot.cpp
@@ -1107,7 +1107,9 @@ void DatabaseSnapshot::putRequest(const jrd_req* request, ClumpletWriter& writer
 		writer.insertTimeStamp(f_mon_stmt_timestamp, empty);
 	}
 	// sql text
-	writer.insertString(f_mon_stmt_sql_text, request->req_sql_text ? *request->req_sql_text : string());
+	const static string emptyString;
+	const string &sql = request->req_sql_text ? (*request->req_sql_text) : emptyString;
+	writer.insertString(f_mon_stmt_sql_text, sql);
 	// statistics
 	writer.insertBigInt(f_mon_stmt_stat_id, getGlobalId(stat_id));
 	putStatistics(request->req_stats, writer, stat_id, stat_statement);
