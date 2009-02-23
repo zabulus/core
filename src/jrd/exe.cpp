890ba0ad3dff4f81e40e78e1988cae5a94b889bb
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-02-23 14:07:28 +0000

    Front ported CORE-2326: AV when committing a new view while trigger on rdb$relations is in place. Actually, this fix is not required for v2.5 due to the DFW logic changes there, but it doesn't hurt either.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@40556 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/exe.cpp
+++ src/jrd/exe.cpp
@@ -1548,7 +1548,8 @@ static void execute_procedure(thread_db* tdbb, jrd_nod* node)
 		Jrd::ContextPoolHolder context(tdbb, proc_request->req_pool);
 
 		jrd_tra* transaction = request->req_transaction;
-		const SLONG save_point_number = transaction->tra_save_point->sav_number;
+		const SLONG save_point_number = transaction->tra_save_point ?
+			transaction->tra_save_point->sav_number : 0;
 
 		proc_request->req_timestamp = request->req_timestamp;
 		EXE_start(tdbb, proc_request, transaction);
