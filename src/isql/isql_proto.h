f964399558031c597ca0ab7902582b557cf8b1ac
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-02-28 12:34:48 +0000

    Little cleanup.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@40644 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/isql/isql_proto.h
+++ src/isql/isql_proto.h
@@ -28,9 +28,9 @@
 
 void	ISQL_array_dimensions(const TEXT*);
 TEXT*	ISQL_blankterm2(const TEXT* input, TEXT* output);
-void	ISQL_build_table_list(void**, FILE*, FILE*, FILE*);
-void	ISQL_build_view_list(void**, FILE*, FILE*, FILE*);
-int		ISQL_commit_work(int, FILE*, FILE*, FILE*);
+//void	ISQL_build_table_list(void**, FILE*, FILE*, FILE*);
+//void	ISQL_build_view_list(void**, FILE*, FILE*, FILE*);
+//int		ISQL_commit_work(int, FILE*, FILE*, FILE*);
 void	ISQL_copy_SQL_id(const TEXT*, TEXT*, TEXT);
 // CVC: Not found, probably in some lost GUI-related file.
 //int		ISQL_create_database(TEXT*,
@@ -66,11 +66,11 @@ void	ISQL_msg_get(USHORT number, USHORT size, TEXT* msg,
 void	ISQL_print_validation(FILE*, ISC_QUAD*, bool, FB_API_HANDLE);
 void	ISQL_printf(FILE*, const char*);
 void	ISQL_printf2(FILE*, const char*, ...);
-void	ISQL_query_database(SSHORT*, FILE*, FILE*, FILE*);
+//void	ISQL_query_database(SSHORT*, FILE*, FILE*, FILE*);
 void	ISQL_remove_and_unescape_quotes(TEXT* string, const char quote);
-void	ISQL_reset_settings();
+//void	ISQL_reset_settings();
 void	ISQL_ri_action_print(const TEXT*, const TEXT*, bool);
-int		ISQL_sql_statement(TEXT*, FILE*, FILE*, FILE*);
+//int		ISQL_sql_statement(TEXT*, FILE*, FILE*, FILE*);
 void	ISQL_truncate_term(TEXT*, USHORT);
 //void	ISQL_win_err(const char*);
 processing_state ISQL_print_item_blob(FILE*, const XSQLVAR*, FB_API_HANDLE, int subtype);
