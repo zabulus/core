9c11dc358952b331b2d0624175ce2e56daaea824
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-04-13 01:46:26 +0000

    Misc
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@41301 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/dsql/pass1.cpp
+++ src/dsql/pass1.cpp
@@ -3474,27 +3474,27 @@ static bool node_match(const dsql_nod* node1, const dsql_nod* node2,
 			const dsql_ctx* ctx1 = (dsql_ctx*) node1->nod_arg[e_derived_table_context];
 			const dsql_ctx* ctx2 = (dsql_ctx*) node2->nod_arg[e_derived_table_context];
 			fb_assert(ctx1 && ctx2);
-			
+
 			if (ctx1->ctx_context != ctx2->ctx_context)
 				return false;
 
-			// hvlad: i'm not sure if we need comparison's below. I.e. if two DT have the 
+			// hvlad: i'm not sure if we need comparisons below. I.e. if two DT have the
 			// same context number how it can be different in other aspects ?
 
 			const dsql_str* alias1 = (dsql_str*) node1->nod_arg[e_derived_table_alias];
 			const dsql_str* alias2 = (dsql_str*) node2->nod_arg[e_derived_table_alias];
-			if (alias1 && !alias2 || !alias1 && alias2)
+			if ((alias1 && !alias2) || (!alias1 && alias2))
 				return false;
 
-			if (alias1 && (alias1->str_charset != alias2->str_charset || 
-				alias1->str_length != alias2->str_length ||
-				strncmp(alias1->str_data, alias2->str_data, alias1->str_length)))
+			if (alias1 && (alias1->str_charset != alias2->str_charset ||
+					alias1->str_length != alias2->str_length ||
+					strncmp(alias1->str_data, alias2->str_data, alias1->str_length)))
 			{
 				return false;
 			}
 
-			return node_match(node1->nod_arg[e_derived_table_rse], node2->nod_arg[e_derived_table_rse], 
-				ignore_map_cast);
+			return node_match(node1->nod_arg[e_derived_table_rse],
+				node2->nod_arg[e_derived_table_rse], ignore_map_cast);
 		}
 	} // switch
 
