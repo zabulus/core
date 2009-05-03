35ecd5320af86df6ac46e9180629f7b4ea075276
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-05-03 22:11:11 +0000

    Looks like some problem with my keyboard :D
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@41777 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/enc.cpp
+++ src/jrd/enc.cpp
@@ -249,7 +249,7 @@ union C_block
 #define	LOADREG(d, d0, d1, s, s0, s1)	d0 = s0, d1 = s1
 #define	OR(d, d0, d1, bl)				d0 |= (bl).b32.i0, d1 |= (bl).b32.i1
 #define	STORE(s, s0, s1, bl)			(bl).b32.i0 = s0, (bl).b32.i1 = s1
-#define	DCL_BLOCK(d, d0, d1)	eee		SLONG d0, d1
+#define	DCL_BLOCK(d, d0, d1)			SLONG d0, d1
 
 	/* "small data" */
 #define	LGCHUNKBITS	2
