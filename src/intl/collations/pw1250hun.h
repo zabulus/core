29f95425e442d7ae72e0c975779bc0ff425fd92f
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-05-05 12:46:23 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@41818 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/intl/collations/pw1250hun.h
+++ src/intl/collations/pw1250hun.h
@@ -32,8 +32,8 @@ const int NULL_SECONDARY		= 0;
 const int NULL_TERTIARY			= 0;
 //const int FIRST_IGNORE			= 1;
 const int FIRST_TERTIARY		= 1;
-const int FIRST_SECONDARY		= (FIRST_TERTIARY+MAX_NCO_TERTIARY+1);
-const int FIRST_PRIMARY			= (FIRST_SECONDARY+MAX_NCO_SECONDARY+1);
+const int FIRST_SECONDARY		= (FIRST_TERTIARY + MAX_NCO_TERTIARY + 1);
+const int FIRST_PRIMARY			= (FIRST_SECONDARY + MAX_NCO_SECONDARY + 1);
 
 static const BYTE ToUpperConversionTbl [ UPPERCASE_LEN ] = {
      0, /*     0 ->     0 */
