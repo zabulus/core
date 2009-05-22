8c76ebde001aa4bc25a3bd20ea0c00d6305213e6
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-05-22 22:40:44 +0000

    Correction
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@42032 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/gds.cpp
+++ src/jrd/gds.cpp
@@ -3685,7 +3685,7 @@ public:
 		Firebird::PathName lockPrefix;
 		if (!fb_utils::readenv(FB_LOCK_ENV, lockPrefix))
 		{
-			PathUtils::concatPath(lockPrefix, WORKFILE, LOCKDIR);
+			PathUtils::concatPath(lockPrefix, fbTempDir, LOCKDIR);
 		}
 		lockPrefix.copyTo(fb_prefix_lock_val, sizeof(fb_prefix_lock_val));
 		fb_prefix_lock = fb_prefix_lock_val;
