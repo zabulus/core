43b9e3e83bb3795c916a908451994bd3384ca0f5
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-05-25 07:32:39 +0000

    Style.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@42067 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/utilities/print_pool.cpp
+++ src/utilities/print_pool.cpp
@@ -23,6 +23,7 @@
 #include "../jrd/ibase.h"
 #include "../jrd/svc_undoc.h"
 #include "../common/stuff.h"
+#include "../common/utils_proto.h"
 
 int CLIB_ROUTINE main( int argc, char **argv)
 {
@@ -35,54 +36,68 @@ int CLIB_ROUTINE main( int argc, char **argv)
 *Functional Description
 *  This utility uses the Firebird service api to inform the server
 *  to print out the memory pool information into a specified file.
-*  is utilitiy is for WIN_NT only, In case of UNIX ibmgr utility will
+*  This utilitiy is for WIN_NT only, In case of UNIX ibmgr utility will
 *  should be used.
 *
 *************************************************************************/
-	char buffer[512];
 	char fname[512];
-	ISC_STATUS_ARRAY status;
 
 	if (argc != 2 && argc != 1) {
 		printf("Usage %s \n      %s filename\n");
 		exit(1);
 	}
-	if (argc == 1) {
+	if (argc == 1)
+	{
 		printf(" Filename : ");
-		gets(fname);
+		if (!fgets(fname, sizeof(fname), stdin))
+			return 1;
+		const size_t len = strlen(fname);
+		if (!len)
+			return 1;
+		if (fname[len - 1] == '\n')
+		{
+			fname[len - 1] = 0;
+			if (len == 1)
+				return 1;
+		}
 	}
 	else
-		strcpy(fname, argv[1]);
+	{
+		fb_utils::copy_terminate(fname, argv[1], sizeof(fname));
+		if (!fname[0])
+			return 1;
+	}
 
-	strcpy(buffer, fname);
-	printf("Filename to dump pool info = %s \n", buffer);
-	char svc_name[256];
-	sprintf(svc_name, "localhost:anonymous");
+	printf("Filename to dump pool info = %s \n", fname);
 
+	ISC_STATUS_ARRAY status;
+
+	const char svc_name[] = "localhost:anonymous";
 	isc_svc_handle svc_handle = NULL;
 	if (isc_service_attach(status, 0, svc_name, &svc_handle, 0, NULL)) {
 		printf("Failed to attach service\n");
-		return 0;
+		return 1;
 	}
 
-	const unsigned short path_length = strlen(buffer);
+	const unsigned short path_length = strlen(fname);
 
-	char sendbuf[512];
+	char sendbuf[520]; // 512 + tag + length_word
 	char* sptr = sendbuf;
 	*sptr = isc_info_svc_dump_pool_info;
 	++sptr;
 	add_word(sptr, path_length);
-	strcpy(sptr, buffer);
+	strcpy(sptr, fname);
 	sptr += path_length;
 
 	char respbuf[256];
-	if (isc_service_query(status, &svc_handle, NULL, 0, NULL, sptr - sendbuf,
-			sendbuf, sizeof(respbuf), respbuf))
+	if (isc_service_query(status, &svc_handle, NULL, 0, NULL,
+		sptr - sendbuf, sendbuf, sizeof(respbuf), respbuf))
 	{
 		printf("Failed to query service\n");
 		isc_service_detach(status, &svc_handle);
-		return 0;
+		return 1;
 	}
 
 	isc_service_detach(status, &svc_handle);
+	return 0;
 }
