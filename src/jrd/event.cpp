2f2068d0226499f557dbb988e79071c13898ad49
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-05-30 14:17:53 +0000

    Misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@42189 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/event.cpp
+++ src/jrd/event.cpp
@@ -1401,13 +1401,13 @@ static void release(void)
 #ifdef MULTI_THREAD
 	EVENT_header->evh_current_process = 0;
 	if (mutex_state = ISC_mutex_unlock(MUTEX))
-		mutex_bugcheck("mutex lock", mutex_state);
+		mutex_bugcheck("mutex unlock", mutex_state);
 #else
 	if (!--acquire_count) {
 		EVENT_header->evh_current_process = 0;
 #ifndef SERVER
 		if (mutex_state = ISC_mutex_unlock(MUTEX))
-			mutex_bugcheck("mutex lock", mutex_state);
+			mutex_bugcheck("mutex unlock", mutex_state);
 #endif
 #ifdef UNIX
 		if (EVENT_process_offset) {
