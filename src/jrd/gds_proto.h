e360a8e58f5e7c55da09447bcf0cbababc54477f
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-06-10 09:48:16 +0000

    cleanup
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@42528 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/gds_proto.h
+++ src/jrd/gds_proto.h
@@ -134,11 +134,6 @@ void	gds__trace_printer(void*, SSHORT, const TEXT*);
 void	gds__print_pool(Firebird::MemoryPool*, const TEXT*, ...);
 void	GDS_init_prefix();
 
-#if (defined SOLARIS && !defined(MAP_ANON))
-UCHAR*   mmap_anon(SLONG);
-#endif
-
-
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
