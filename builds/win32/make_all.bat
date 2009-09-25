aa9c81e2cbacf305bcfbe8a81446a4af3979bd19
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-09-25 13:38:46 +0000

    Make the best of a bad situation. Using find /V will get messy if we have to strip several markers.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@44760 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/make_all.bat
+++ builds/win32/make_all.bat
@@ -66,7 +66,7 @@ for %%v in ( btyacc gbak_embed gpre_boot gpre_embed isql_embed ) do (
 :: Firebird.conf, etc
 @copy %FB_GEN_DIR%\firebird.msg %FB_OUTPUT_DIR% > nul
 :: The line @UDF_COMMENT@ should be deleted from the target file.
-@copy %FB_ROOT_PATH%\builds\install\misc\firebird.conf.in %FB_OUTPUT_DIR%\firebird.conf >nul
+find /V "@UDF_COMMENT@" %FB_ROOT_PATH%\builds\install\misc\firebird.conf.in > %FB_OUTPUT_DIR%\firebird.conf
 @copy %FB_ROOT_PATH%\builds\install\misc\fbintl.conf %FB_OUTPUT_DIR%\intl >nul
 @copy %FB_ROOT_PATH%\src\utilities\ntrace\fbtrace.conf %FB_OUTPUT_DIR% >nul
 @copy %FB_ROOT_PATH%\builds\install\misc\IPLicense.txt %FB_OUTPUT_DIR% >nul
