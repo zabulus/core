7d8c5616422589a1165166b00872031ddbaa2713
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2009-12-25 13:30:38 +0000

    PSQL functions (continued).
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@47344 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/irq.h
+++ src/jrd/irq.h
@@ -122,6 +122,9 @@ enum irq_type_t
 	irq_m_fields4,			// process a modification of RDB$FIELDS for procedures (TYPE OF COLUMN)
 	irq_m_fields5,			// process a modification of RDB$FIELDS for triggers (TYPE OF COLUMN)
 	irq_m_fields6,			// process a modification of RDB$FIELDS for packaged procedures (TYPE OF COLUMN)
+	irq_m_fields7,			// process a modification of RDB$FIELDS for functions
+	irq_m_fields8,			// process a modification of RDB$FIELDS for functions (TYPE OF COLUMN)
+	irq_m_fields9,			// process a modification of RDB$FIELDS for packaged functions (TYPE OF COLUMN)
 	irq_l_colls,			// lookup collations
 	irq_l_relfield,			// lookup a relation field
 	irq_verify_trusted_role, // ensure trusted role exists
