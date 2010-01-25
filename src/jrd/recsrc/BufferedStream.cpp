34dcab38693ba1afba45821a862f86a8c994c2fc
robocop <robocop@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-01-25 06:48:09 +0000

    Totally misc.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/trunk@47999 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/recsrc/BufferedStream.cpp
+++ src/jrd/recsrc/BufferedStream.cpp
@@ -159,7 +159,7 @@ bool BufferedStream::getRecord(thread_db* tdbb)
 		{
 			// ASF: There is nothing more to read, so remove irsb_mustread flag.
 			// That's important if m_next is reused in another stream and our caller
-			// rely on this BufferedStream being non-lazy, like WindowedStream does.
+			// relies on this BufferedStream being non-lazy, like WindowedStream does.
 			impure->irsb_flags &= ~irsb_mustread;
 			return false;
 		}
