142d6f8ba79325b1cf54b83e5b2d7a21b4d3fafa
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-02-16 14:39:38 +0000

    Fixed CORE-1089: Wrong ordering with views, distinct, left join and order by and CORE-2871: Outer ORDER BY clause has no effect.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_1_Release@48546 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/opt.cpp
+++ src/jrd/opt.cpp
@@ -1647,8 +1647,10 @@ static void check_sorts(RecordSelExpr* rse)
 
 					// AB: Don't distribute the sort when a FIRST/SKIP is supplied,
 					// because that will affect the behaviour from the deeper RSE.
+					// dimitr: the same rule applies to explicit/implicit user-defined sorts.
 					if (new_rse != rse &&
-						(new_rse->rse_first || new_rse->rse_skip))
+						(new_rse->rse_first || new_rse->rse_skip ||
+						new_rse->rse_sorted || new_rse->rse_projection))
 					{
 						node = NULL;
 						break;
