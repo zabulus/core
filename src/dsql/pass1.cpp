683538dac1a390ddf8752af172c0b007d5bf722f
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-02-28 14:42:04 +0000

    Backport fix for CORE-2888 - A memory corruption cause incorrect query evaluation and may crash the server
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_5_Release@48722 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/dsql/pass1.cpp
+++ src/dsql/pass1.cpp
@@ -7053,8 +7053,13 @@ static dsql_nod* pass1_make_derived_field(CompiledStatement* statement, thread_d
 				// Try to generate derived field from sub-select
 				dsql_nod* derived_field = pass1_make_derived_field(statement, tdbb,
 					select_item->nod_arg[e_via_value_1]);
-				derived_field->nod_arg[e_derived_field_value] = select_item;
-				return derived_field;
+				if (derived_field->nod_type == nod_derived_field)
+				{
+					derived_field->nod_arg[e_derived_field_value] = select_item;
+					return derived_field;
+				}
+
+				return select_item;
 			}
 
 		default:
