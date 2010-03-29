77cfc140db8ccf4d5d83b8cf69eb5771a404ef75
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-03-29 07:23:21 +0000

    Update.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_5_Release@49330 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -3,13 +3,13 @@
 # This file is used both to rebuild the header file and to set the
 # environment variables on the config call
 
-BuildVersion="$Id: writeBuildNum.sh,v 1.25898.2.12 2010-03-13 10:39:26 dimitr Exp $"
+BuildVersion="$Id: writeBuildNum.sh,v 1.25898.2.13 2010-03-29 07:22:41 dimitr Exp $"
 
 BuildType=V
 MajorVer=2
 MinorVer=5
 RevNo=0
-BuildNum=25958
+BuildNum=25970
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
 # Normal builds
