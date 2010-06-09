ec4002c258fea7db3460c19724406d77e979aca9
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-06-09 11:16:10 +0000

    backported fixes, required to build using new autotools
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_0_Release@51234 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- autogen.sh
+++ autogen.sh
@@ -8,44 +8,25 @@ PKG_NAME=Firebird2
 SRCDIR=`dirname $0`
 DIE=0
 
-if [ -z "$AUTOCONF" ]
+if [ -z "$AUTORECONF" ]
 then
-  AUTOCONF=autoconf
-fi
-if [ -z "$LIBTOOL" ]
-then
-  LIBTOOL=libtool
-fi
-if [ -z "$LIBTOOLIZE" ]
-then
-  LIBTOOLIZE=libtoolize
+  AUTORECONF=autoreconf
 fi
 
-echo "AUTOCONF="$AUTOCONF
-echo "LIBTOOL="$LIBTOOL
-echo "LIBTOOLiZE="$LIBTOOLIZE
-AUTOHEADER=`echo $AUTOCONF |sed 's/conf/header/'`
+echo "AUTORECONF="$AUTORECONF
 
-VER=`$AUTOCONF --version|grep '^[Aa]utoconf'|sed 's/^[^0-9]*//'`
+# This prevents calling automake in old autotools
+AUTOMAKE=true
+export AUTOMAKE
+
+VER=`$AUTORECONF --version|grep '^[Aa]utoreconf'|sed 's/^[^0-9]*//'`
 case "$VER" in
  0* | 1\.* | 2\.[0-9] | 2\.[0-9][a-z]* | \
- 2\.[1-4][0-9] | 2\.5[0-5][a-z]* )
+ 2\.[1-4][0-9] | 2\.[1-4][0-9][a-z]* | 2\.5[0-5] | 2\.5[0-5][a-z]* )
   echo
   echo "**Error**: You must have autoconf 2.56 or later installed."
   echo "Download the appropriate package for your distribution/OS,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/"
-  DIE=1
-  ;;
-esac
-
-VER=`$LIBTOOL --version|grep ' libtool)'|sed 's/.*) \([0-9][0-9.]*\) .*/\1/'`
-case "$VER" in
- 0* | 1\.[0-2] | 1\.[0-2][a-z]* | \
- 1\.3\.[0-2] | 1\.3\.[0-2][a-z]* )
-  echo
-  echo "**Error**: You must have libtool 1.3.3 or later installed."
-  echo "Download the appropriate package for your distribution/OS,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/"
   DIE=1
   ;;
 esac
@@ -66,20 +47,36 @@ if test -z "$*" -a x$NOCONFIGURE = x; then
   echo
 fi
 
-# Generate configure from configure.in
-echo "Running libtoolize ..."
-LIBTOOL_M4=`$LIBTOOLIZE --copy --force --dry-run|grep 'You should add the contents of'|sed "s,^[^/]*\(/[^']*\).*$,\1,"`
-if test "x$LIBTOOL_M4" != "x"; then
- rm -f aclocal.m4
- cp $LIBTOOL_M4 aclocal.m4
+# Some versions of autotools need it
+if [ ! -d m4 ]; then
+	rm -rf m4
+	mkdir m4
 fi
-$LIBTOOLIZE --copy --force || exit 1
 
-echo "Running autoheader ..."
-$AUTOHEADER || exit 1
+# Ensure correct utilities are called by AUTORECONF
+autopath=`dirname $AUTORECONF`
+if [ "x$autopath" != "x" ]; then
+	PATH=$autopath:$PATH
+	export PATH
+fi
 
-echo "Running autoconf ..."
-$AUTOCONF || exit 1
+# Generate configure from configure.in
+echo "Running autoreconf ..."
+$AUTORECONF --install --force --verbose || exit 1
+
+# Hack to bypass bug in autoreconf - --install switch not passed to libtoolize,
+# therefore missing config.sub and confg.guess files
+CONFIG_AUX_DIR=builds/make.new/config
+if [ ! -f $CONFIG_AUX_DIR/config.sub -o ! -f $CONFIG_AUX_DIR/config.guess ]; then
+	# re-run libtoolize with --install switch, if it does not understand that switch
+	# and there are no config.sub/guess files in CONFIG_AUX_DIR, we will anyway fail
+	echo "Re-running libtoolize ..."
+	if [ -z "$LIBTOOLIZE" ]; then
+		LIBTOOLIZE=libtoolize
+	fi
+	$LIBTOOLIZE --install --copy --force || exit 1
+fi
+
 
 # If NOCONFIGURE is set, skip the call to configure
 if test "x$NOCONFIGURE" = "x"; then
