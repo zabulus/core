c8e29e10941b8591dc9486b55fcfaf82b675c2e6
dimitr <dimitr@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-07-08 10:44:35 +0000

    Fixed the old bug which was left unnoticed until RC3 where Pair<MetaName, MetaName> was used as a key for a b-tree. At the first glance, it promises only wrong ordering of items, but in fact it trashes the b-tree internals and crashes the server :-( It's unregistered and undocumented, but being a very recent RC3 only regression, let's leave it this way. I suppose the intended code should be !(v.first < this->first), but we have the operator == defined, so I used a simpler form.
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_1_Release@51317 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/common/classes/fb_pair.h
+++ src/common/classes/fb_pair.h
@@ -119,7 +119,7 @@ template<typename BasePair>
 		}
 		bool operator<(const Pair& v) const
 		{
-			return this->first < v.first || (!(this->first < v.first) && this->second < v.second); 
+			return this->first < v.first || (this->first == v.first && this->second < v.second); 
 		}
 		bool operator!=(const Pair& v) const
 		{
