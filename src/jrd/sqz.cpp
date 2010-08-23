19d2bda4ee6bc1cde07176f7c1ca8a9815744074
alexpeshkoff <alexpeshkoff@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2010-08-23 10:06:11 +0000

    Applied suggested by Dmitry Kovalenko fix - it helps avoid AV when buffer is allocated exactly at the end of physical memory segment
    git-svn-id: file:///home/asfernandes/fb/repository/sf/svn/firebird/branches/B2_5_Release@51461 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/sqz.cpp
+++ src/jrd/sqz.cpp
@@ -219,12 +219,12 @@ UCHAR* SQZ_decompress(const UCHAR*	input,
 		const int len = (signed char) *input++;
 		if (len < 0)
 		{
-			const UCHAR c = *input++;
-
-			if ((output - len) > output_end)
+			if (input >= last || (output - len) > output_end)
 			{
 				BUGCHECK(179);	/* msg 179 decompression overran buffer */
 			}
+
+			const UCHAR c = *input++;
 			memset(output, c, (-1 * len));
 			output -= len;
 		}
@@ -336,7 +336,7 @@ USHORT SQZ_differences(const SCHAR*	rec1,
 			   when rec1 is at the end of a segment, to avoid wrapping around */
 
 			const SCHAR* yellow = (SCHAR *) MIN((U_IPTR) end1, ((U_IPTR) rec1 + 127)) - 1;
-			while (rec1 <= yellow && (rec1[0] != rec2[0] || (rec1[1] != rec2[1] && rec1 < yellow)))
+			while (rec1 <= yellow && (rec1[0] != rec2[0] || (rec1 < yellow && rec1[1] != rec2[1])))
 			{
 				STUFF(*rec2++);
 				++rec1;
