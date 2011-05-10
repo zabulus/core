266cfbf918191ee6abdd095b6e40277c95d8f86e
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2011-05-10 13:37:52 +0000

    Don't use Guardian with services
    git-svn-id: https://firebird.svn.sourceforge.net/svnroot/firebird/firebird/trunk@52911 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/install/arch-specific/win32/install_super.bat
+++ builds/install/arch-specific/win32/install_super.bat
@@ -3,7 +3,7 @@ if "%1"=="" (
 instreg install -z
 )
 setlocal
-set INSTALL_SERVICE=install -auto -superserver -guardian -z
+set INSTALL_SERVICE=install -auto -superserver -z
 set START_SERVICE=start
 if not "%1"=="" (
 set INSTALL_SERVICE=%INSTALL_SERVICE% -n %1
