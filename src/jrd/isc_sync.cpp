ca7b15679240a001cc022adb8cb3fd189437d0ff
alexpeshkoff <alexpeshkoff@65644016-39b1-43b1-bf79-96bc8fe82c15>
2011-07-01 10:15:49 +0000

    This appears to fix CORE-3400: Server crash every 3 hour (FreeBSD8.2R), but I did not get final confirmation from the reporter too long
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_5_Release@53269 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/jrd/isc_sync.cpp
+++ src/jrd/isc_sync.cpp
@@ -320,8 +320,8 @@ namespace {
 	public:
 		const static int N_FILES = 8;
 		const static int N_SETS = 256;
-#if defined(DEV_BUILD) || defined(FREEBSD)
-		const static int SEM_PER_SET = 4;	// force multiple sets allocation || work with default freebsd kernel
+#if defined(DEV_BUILD)
+		const static int SEM_PER_SET = 4;	// force multiple sets allocation
 #else
 		const static int SEM_PER_SET = 31;	// hard limit for some old systems, might set to 32
 #endif
