b52108fa4556b6426063ea543722d6a91e31f128
asfernandes <asfernandes@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2011-09-17 20:29:57 +0000

    Correction for future use of UTF-8 metadata.
    git-svn-id: https://firebird.svn.sourceforge.net/svnroot/firebird/firebird/trunk@53447 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/fields.h
+++ src/jrd/fields.h
@@ -170,4 +170,4 @@
 	FIELD(fld_user_name		, nam_user_name		, dtype_text	, MAX_SQL_IDENTIFIER_LEN	, dsc_text_type_metadata	, NULL		, true)
 	FIELD(fld_uid			, nam_uid			, dtype_long	, sizeof(SLONG)				, 0							, NULL		, true)
 	FIELD(fld_gid			, nam_gid			, dtype_long	, sizeof(SLONG)				, 0							, NULL		, true)
-	FIELD(fld_name_part		, nam_name_part		, dtype_text	, 32						, dsc_text_type_metadata	, NULL		, true)
+	FIELD(fld_name_part		, nam_name_part		, dtype_text	, MAX_SQL_IDENTIFIER_LEN	, dsc_text_type_metadata	, NULL		, true)
