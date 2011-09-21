acb798217e0f194c9e7b3609b93e2bcb9e73ff41
alexpeshkoff <alexpeshkoff@65644016-39b1-43b1-bf79-96bc8fe82c15>
2011-09-21 13:51:26 +0000

    Fixed unregistered bug - regression, found in 2.5.1 rc1
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_5_Release@53460 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/remote/inet.cpp
+++ src/remote/inet.cpp
@@ -346,7 +346,7 @@ public:
 #endif
 	}
 
-	int select(timeval* timeout)
+	void select(timeval* timeout)
 	{
 		bool hasRequest = false;
 
@@ -364,28 +364,28 @@ public:
 		if (!hasRequest)
 		{
 			errno = NOTASOCKET;
-			return -1;
+			slct_count = -1;
+			return;
 		}
 
 		int milliseconds = timeout ? timeout->tv_sec * 1000 + timeout->tv_usec / 1000 : -1;
 		slct_count = ::poll(slct_poll.begin(), slct_poll.getCount(), milliseconds);
 
-		for (pollfd* pf = slct_poll.begin(); pf < end; ++pf)
+		if (slct_count >= 0)	// in case of error return revents may contain something bad
 		{
-			pf->events = pf->revents;
+			for (pollfd* pf = slct_poll.begin(); pf < end; ++pf)
+			{
+				pf->events = pf->revents;
+			}
 		}
 #else
 #ifdef WIN_NT
 		slct_count = ::select(FD_SETSIZE, &slct_fdset, NULL, NULL, timeout);
 #else
-
-
 		slct_count = ::select(slct_width, &slct_fdset, NULL, NULL, timeout);
 #endif // WIN_NT
 #endif // HAVE_POLL
 
-
-		return slct_count;
 	}
 
 	int getCount()
