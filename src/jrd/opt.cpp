7312dc710d69adadf2da1606b8aa94ecc6754160
alexpeshkoff <alexpeshkoff@65644016-39b1-43b1-bf79-96bc8fe82c15>
2012-01-19 15:34:03 +0000

    Backported fix for CORE-3736: WITH LOCK clause is allowed for users with read-only rights on some table, thus blocking others from updating this table
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_5_Release@53893 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/jrd/opt.cpp
+++ src/jrd/opt.cpp
@@ -932,6 +932,25 @@ RecordSource* OPT_compile(thread_db*		tdbb,
 		rsb = gen_first(tdbb, opt, rsb, rse->rse_first);
 	}
 
+	if (rse->rse_writelock)
+	{
+		rsb->rsb_flags |= rsb_writelock;
+
+		for (USHORT i = 1; i <= streams[0]; ++i)
+		{
+			const USHORT stream = streams[i];
+			CompilerScratch::csb_repeat* r = &csb->csb_rpt[stream];
+			r->csb_flags |= csb_update;
+
+			if (r->csb_relation)
+			{
+				CMP_post_access(tdbb, csb, r->csb_relation->rel_security_name,
+					r->csb_view ? r->csb_view->rel_id : 0,
+					SCL_sql_update, "TABLE", r->csb_relation->rel_name);
+			}
+		}
+	}
+
 	// release memory allocated for index descriptions
 	for (USHORT i = 1; i <= streams[0]; ++i)
 	{
@@ -971,17 +990,6 @@ RecordSource* OPT_compile(thread_db*		tdbb,
 		}
 		delete opt;
 		throw;
-	}
-
-	if (rse->rse_writelock)
-	{
-		rsb->rsb_flags |= rsb_writelock;
-
-		for (USHORT i = 1; i <= streams[0]; ++i)
-		{
-			const USHORT stream = streams[i];
-			csb->csb_rpt[stream].csb_flags |= csb_update;
-		}
 	}
 
 	// Assign pointer to list of dependent invariant values
