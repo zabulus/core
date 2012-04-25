05fb29249f67b7d7fd8d83e5ee88aa4daf610133
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2012-04-25 08:32:30 +0000

    Correct syntax to make it works on Windows
    git-svn-id: https://firebird.svn.sourceforge.net/svnroot/firebird/firebird/trunk@54404 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/parse.bat
+++ builds/win32/parse.bat
@@ -9,9 +9,9 @@
 
 @echo Generating parse.cpp and dsql.tab.h
 
-@sed -n '/%type .*/p' < %FB_ROOT_PATH%\src\dsql\parse.y > y.types
-@sed 's/%type .*//' < %FB_ROOT_PATH%\src\dsql\parse.y > y.y
-@sed $(INLINE_EDIT_SED) '/\/*\*\* TYPES \*\*\*\//r y.types' y.y
+@sed -n "/%%type .*/p" < %FB_ROOT_PATH%\src\dsql\parse.y > y.types
+@sed "s/%%type .*//" < %FB_ROOT_PATH%\src\dsql\parse.y > y.y
+@sed -i "/\/*\*\* TYPES \*\*\*\//r" y.types y.y
 
 %FB_ROOT_PATH%\temp\%FB_OBJ_DIR%\btyacc\btyacc -l -d -S %FB_ROOT_PATH%\src\dsql\btyacc_fb.ske y.y
 @if errorlevel 1 (exit /B 1)
