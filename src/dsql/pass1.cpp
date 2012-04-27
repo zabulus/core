932097656b211e741aecd209278c22ca78d16f79
dimitr <dimitr@65644016-39b1-43b1-bf79-96bc8fe82c15>
2012-04-27 16:31:24 +0000

    Fixed CORE-3834: Usage of a NATURAL JOIN with a derived table crashes the server.
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_5_Release@54428 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/dsql/pass1.cpp
+++ src/dsql/pass1.cpp
@@ -6720,7 +6720,7 @@ static dsql_nod* pass1_join(CompiledStatement* statement, dsql_nod* input)
 				coalesce->nod_arg[1] = MAKE_list(stack);
 
 				impJoinLeft->value = MAKE_node(nod_alias, e_alias_count);
-				impJoinLeft->value->nod_arg[e_alias_value] = coalesce;
+				impJoinLeft->value->nod_arg[e_alias_value] = PASS1_node(statement, coalesce);
 				impJoinLeft->value->nod_arg[e_alias_alias] = reinterpret_cast<dsql_nod*>(fldName);
 				impJoinLeft->value->nod_arg[e_alias_imp_join] = reinterpret_cast<dsql_nod*>(impJoinLeft);
 
