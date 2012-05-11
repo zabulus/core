5e1d0970ac58c9edefe7e9ce95c351aa25cbf615
hvlad <hvlad@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2012-05-11 19:39:11 +0000

    Fixed bug CORE-3841 : Corrupted database after inserting rows. It should also fix CORE-3839 : Values disappearing from index
    git-svn-id: https://firebird.svn.sourceforge.net/svnroot/firebird/firebird/trunk@54470 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- src/jrd/btr.cpp
+++ src/jrd/btr.cpp
@@ -4086,6 +4086,7 @@ static UCHAR* find_area_start_point(btree_page* bucket, const temporary_key* key
 
 							if (node.length != 0 ||
 								node.prefix != prevJumpNode.prefix + prevJumpNode.length ||
+								node.prefix < jumpKey.key_length ||
 								jumpNode.prefix != prevJumpNode.prefix + prevJumpNode.length ||
 								node.isEndBucket || node.isEndLevel)
 							{
