85f214bdda8074a7f97ef600859a4d3543056c41
paul_reeves <paul_reeves@de594faa-8d1b-4a0c-9a6a-a7de5f8bf859>
2012-06-07 10:08:46 +0000

    Add option to just build (default is to do make packages after build.)
    git-svn-id: https://firebird.svn.sourceforge.net/svnroot/firebird/firebird/trunk@54616 de594faa-8d1b-4a0c-9a6a-a7de5f8bf859

--- builds/win32/run_all.bat
+++ builds/win32/run_all.bat
@@ -7,6 +7,7 @@ set FB_NOCLEAN=
 set FBBUILD_BUILDTYPE=release
 set FBBUILD_INCLUDE_PDB=
 set FBBUILD_MAKE_KITS_ONLY=
+set FBBUILD_BUILD_ONLY=0
 
 ::Check if on-line help is required
 for %%v in ( %1 %2 %3 %4 %5 %6 %7 %8 %9 )  do (
@@ -22,6 +23,7 @@ for %%v in ( %* )  do (
 ( if /I "%%v"=="DEBUG" (set FBBUILD_BUILDTYPE=debug) )
 ( if /I "%%v"=="PDB" (set FBBUILD_INCLUDE_PDB=1) )
 ( if /I "%%v"=="REPACK" (set FBBUILD_MAKE_KITS_ONLY=1) )
+( if /I "%%v"=="JUSTBUILD" (set FBBUILD_BUILD_ONLY=1) )
 )
 
 if defined FBBUILD_MAKE_KITS_ONLY (goto :MAKE_KITS & goto :EOF)
@@ -37,6 +39,8 @@ call make_all %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
 call make_examples %FBBUILD_BUILDTYPE%
 if "%ERRLEV%"=="1" goto :END
+
+if "%FBBUILD_BUILD_ONLY%"=="1" goto :END
 
 :MAKE_KITS
 :: Package everything up
@@ -55,14 +59,16 @@ goto :END
 @echo.
 @echo The following params may be passed:
 @echo.
-@echo    NOCLEAN - don't run CLEAN_ALL.BAT
+@echo    NOCLEAN   - don't run CLEAN_ALL.BAT
 @echo.
-@echo    DEBUG   - Do a DEBUG build (for experienced developers only.)
-@echo              This switch is not needed to debug Firebird.
+@echo    DEBUG     - Do a DEBUG build (for experienced developers only.)
+@echo                This switch is not needed to debug Firebird.
 @echo.
-@echo    PDB     - Create PDB packages as well as standard kits
+@echo    PDB       - Create PDB packages as well as standard kits
 @echo.
-@echo    REPACK  - Don't build - just repack kits.
+@echo    REPACK    - Don't build - just repack kits.
+@echo.
+@echo    JUSTBUILD - Just build - don't create packages.
 @echo.
 @goto :EOF
 
