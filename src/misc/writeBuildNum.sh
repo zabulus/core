8ccfc75ca964d88ce489528eb4ea490de01f2748
dimitr <dimitr@65644016-39b1-43b1-bf79-96bc8fe82c15>
2012-09-11 14:46:51 +0000

    Bumped the version number.
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_5_Release@57131 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -8,7 +8,7 @@ BuildVersion="$Id: writeBuildNum.sh,v 1.25898.2.19 2010/05/28 14:38:22 alexpeshk
 BuildType=V
 MajorVer=2
 MinorVer=5
-RevNo=2
+RevNo=3
 BuildNum=26538
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
