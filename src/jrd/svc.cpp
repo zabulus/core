3fbefcf3f40c218e77e34d1be9a7e226a1b5252b
alexpeshkoff <alexpeshkoff@65644016-39b1-43b1-bf79-96bc8fe82c15>
2013-07-15 12:24:40 +0000

    Fixed error "All services except for getting server log require switches"
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@58350 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/jrd/svc.cpp
+++ src/jrd/svc.cpp
@@ -2546,8 +2546,6 @@ bool Service::actionNeedsArg(UCHAR action)
 	switch (action)
 	{
 	case isc_action_svc_get_fb_log:
-	case isc_action_svc_trace_list:
-	case isc_action_svc_display_user_adm:
 		return false;
 	}
 	return true;
@@ -2562,9 +2560,6 @@ bool Service::process_switches(ClumpletReader& spb, string& switches)
 	spb.rewind();
 	const UCHAR svc_action = spb.getClumpTag();
 	spb.moveNext();
-
-	if (spb.isEof() && actionNeedsArg(svc_action))
-		return false;
 
 	string burp_database, burp_backup;
 	int burp_options = 0;
