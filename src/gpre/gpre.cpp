841131cc5c97c00da5c74b87b0a32e9b48d42916
robocop <robocop@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-02-22 22:23:31 +0000

    Logic error (PVS-Studio).
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B2_1_Release@59169 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/gpre/gpre.cpp
+++ src/gpre/gpre.cpp
@@ -1925,7 +1925,7 @@ static bool get_switches(int			argc,
 				return false;
 			}
 
-			if (**argv != 'n' || **argv != 'N') {
+			if (**argv != 'n' && **argv != 'N') {
 				fprintf(stderr,
 						   "-sqlda :  Deprecated Feature: you must use XSQLDA\n ");
 				print_switches();
