c370b0978645039759047a8af92cd658d3e046d0
firebirds <firebirds@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-02-25 01:37:54 +0000

    nightly update
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/branches/B1_5_Release@59184 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=V
 MajorVer=1
 MinorVer=5
 RevNo=6
-BuildNum=5038
+BuildNum=5039
 
 if [ "$SPECIAL_BUILD_SUFFIX" = "" ]; then
 # Normal builds
@@ -91,7 +91,7 @@ cat >$1 <<eof
 MajorVer = $MajorVer
 MinorVer = $MinorVer
 RevNo = $RevNo
-BuildNum=5038
+BuildNum=5039
 BuildType = $BuildType
 BuildSuffix = $BuildSuffix
 
