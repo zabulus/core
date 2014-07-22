360d1777f92fd73988e17b2b51f31bf9a1d8245a
skidder <skidder@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-07-22 18:44:22 +0000

    Revert my change, as Dmitry fixed the same error a before me
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@59903 65644016-39b1-43b1-bf79-96bc8fe82c15

--- builds/win32/make_all.bat
+++ builds/win32/make_all.bat
@@ -123,7 +123,6 @@ copy %FB_ROOT_PATH%\src\extlib\fbudf\fbudf.sql %FB_OUTPUT_DIR%\udf > nul
 
 :: MSVC runtime
 if %MSVC_VERSION% == 10 (
-@copy "%VS100COMNTOOLS%\..\..\VC\redist\%FB_VC10CRT_DIR%\Microsoft.VC100.CRT\msvcp100.dll" %FB_OUTPUT_DIR% >nul
 @copy "%VS100COMNTOOLS%\..\..\VC\redist\%FB_VC10CRT_DIR%\Microsoft.VC100.CRT\msvcr100.dll" %FB_OUTPUT_DIR% >nul
 @copy "%VS100COMNTOOLS%\..\..\VC\redist\%FB_VC10CRT_DIR%\Microsoft.VC100.CRT\msvcp100.dll" %FB_OUTPUT_DIR% >nul
 ) else (
