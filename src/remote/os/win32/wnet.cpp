a7d717098f1b56e4745f605c971367a3f02bab1d
alexpeshkoff <alexpeshkoff@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-09-03 13:51:48 +0000

    Fixed VC10 build
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@60029 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/remote/os/win32/wnet.cpp
+++ src/remote/os/win32/wnet.cpp
@@ -1332,9 +1332,9 @@ static bool packet_receive(rem_port* port, UCHAR* buffer, SSHORT buffer_length, 
 	{
 		LocalStatus st;
 		port->port_crypt_plugin->decrypt(&st, n, buffer, buffer);
-		if (!st.isSuccess())
+		if (st.getStatus() & IStatus::FB_HAS_ERRORS)
 		{
-			status_exception::raise(st.get());
+			status_exception::raise(&st);
 		}
 	}
 
@@ -1371,9 +1371,9 @@ static bool packet_send( rem_port* port, const SCHAR* buffer, SSHORT buffer_leng
 
 		char* d = b.getBuffer(buffer_length);
 		port->port_crypt_plugin->encrypt(&st, buffer_length, data, d);
-		if (!st.isSuccess())
+		if (st.getStatus() & IStatus::FB_HAS_ERRORS)
 		{
-			status_exception::raise(st.get());
+			status_exception::raise(&st);
 		}
 
 		data = d;
