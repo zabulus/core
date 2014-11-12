e66c9de7530f6f99c01d9d3f238908a52284f700
mapopa <mapopa@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-11-12 17:49:41 +0000

    add isc_info_db_impl_linux_ppc64 in inf_pub.h
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@60222 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/jrd/inf_pub.h
+++ src/jrd/inf_pub.h
@@ -229,6 +229,8 @@ enum  info_db_implementations
 	isc_info_db_impl_linux_alpha = 83,
 	isc_info_db_impl_linux_arm64 = 84,
 	isc_info_db_impl_linux_ppc64el = 85,
+	isc_info_db_impl_linux_ppc64 = 86,
+
 
 	isc_info_db_impl_last_value   // Leave this LAST!
 };
