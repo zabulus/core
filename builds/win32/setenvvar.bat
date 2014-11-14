004b93f2a7c227fd9e2bbf325f8ab9abeb7863ab
hvlad <hvlad@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-11-14 11:04:48 +0000

    Support for MSVC12 (VS 2013)
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@60236 65644016-39b1-43b1-bf79-96bc8fe82c15

--- builds/win32/setenvvar.bat
+++ builds/win32/setenvvar.bat
@@ -28,6 +28,10 @@ set FB_PROCESSOR_ARCHITECTURE=%PROCESSOR_ARCHITECTURE%
 
 :: To disable VS8/VS9/VS10 build, slightly alter the env var names in "if" conditions below
 
+if DEFINED VS120COMNTOOLS (
+@devenv /? >nul 2>nul
+@if errorlevel 9009 (call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE%) else ( echo    The file: & @echo      "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE% & echo    has already been executed.)
+) else (
 if DEFINED VS100COMNTOOLS (
 @devenv /? >nul 2>nul
 @if errorlevel 9009 (call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE%) else ( echo    The file: & @echo      "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %FB_PROCESSOR_ARCHITECTURE% & echo    has already been executed.)
@@ -45,6 +49,7 @@ if DEFINED VS71COMNTOOLS (
 @if errorlevel 9009 (call "%VS71COMNTOOLS%vsvars32.bat") else ( echo    The file: & echo      "%VS71COMNTOOLS%vsvars32.bat" & echo    has already been executed.)
 ) else (
 @goto :HELP
+)
 )
 )
 )
--- src/common/os/win32/mod_loader.cpp
+++ src/common/os/win32/mod_loader.cpp
@@ -96,7 +96,11 @@ public:
 #elif _MSC_VER == 1500
                     "msvcr90.dll",
 #elif _MSC_VER == 1600
-                    "msvcr100.dll",
+				"msvcr100.dll",
+#elif _MSC_VER == 1700
+				"msvcr110.dll",
+#elif _MSC_VER == 1800
+				"msvcr120.dll",
 #else
                     #error Specify CRT DLL name here !
 #endif
