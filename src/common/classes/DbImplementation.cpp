fa94860f97fc8bbf070e2a9e6c31905515829b8f
mapopa <mapopa@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-11-17 20:26:01 +0000

    fix idpl license url
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@60255 65644016-39b1-43b1-bf79-96bc8fe82c15

--- src/common/classes/DbImplementation.cpp
+++ src/common/classes/DbImplementation.cpp
@@ -7,7 +7,7 @@
  *  Developer's Public License Version 1.0 (the "License");
  *  you may not use this file except in compliance with the
  *  License. You may obtain a copy of the License at
- *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *  https://www.ibphoenix.com/about/firebird/idpl.
  *
  *  Software distributed under the License is distributed AS IS,
  *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
