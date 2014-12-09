c7e0cc3ae0ccdfabce391d0daec22bb26471ad06
firebirds <firebirds@65644016-39b1-43b1-bf79-96bc8fe82c15>
2014-12-09 11:13:39 +0000

    Make it work faster
    git-svn-id: svn+ssh://svn.code.sf.net/p/firebird/code/firebird/trunk@60351 65644016-39b1-43b1-bf79-96bc8fe82c15

--- mkChangeLog.sh
+++ mkChangeLog.sh
@@ -3,15 +3,18 @@
 PositiveOffset=15462	# will be correct for all branches after FB3
 			# Other values are: 2.5:13820 2.1:9165 2.0:6144 1.5:1748
 
-TempLog=temp.log
+TmpDir=/var/tmp
+TmpLog=${TmpDir}/temp.log
 TmpFile=temp.build.num
+TmpSmall=${TmpDir}/small.log
 WriteBuildNumFile="src/misc/writeBuildNum.sh"
 HeaderFile="src/jrd/build_no.h"
 
 cd /home/fbadmin/changelogs/trunk
 svn up || exit
-svn log -v >$TempLog || exit
-smallog <$TempLog >ChangeLog || exit
+svn log -v >$TmpLog || exit
+smallog <$TmpLog >$TmpSmall || exit
+dd if=$TmpSmall of=ChangeLog bs=512k || exit
 
 VersionCount=`egrep -c '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}' ChangeLog`
 BuildNo=$((${VersionCount}+${PositiveOffset}))
@@ -24,4 +27,4 @@ chmod +x $WriteBuildNumFile
 $WriteBuildNumFile rebuildHeader $HeaderFile $TmpFile
                         
 svn commit -m "nightly update" ChangeLog $WriteBuildNumFile $HeaderFile
-rm -f $TempLog $TmpFile
+rm -f $TmpLog $TmpFile $TmpSmall
